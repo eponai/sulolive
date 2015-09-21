9d3dd2ade68038c6f02ad452f7258caaf4865968
Diana Gren <dianagren@gmail.com>
2015-09-21 17:28:15 +0200

    Fix key-intersection with tests.

--- src/server/flipmunks/budget/core.clj
+++ src/server/flipmunks/budget/core.clj
@@ -2,11 +2,51 @@
   (:gen-class)
   (:require clojure.set))
 
-(defn key-sets[& m]
-  (map (into #{} (keys %)) m))
+(defn filter-map-keys [m]
+  (into #{} (map first (filter #(map? (second %)) m))))
 
-(defn deep-merge [& maps]
-  (reduce #(conj %1 %2) maps))
+(defn key-intersection [maps]
+  (apply clojure.set/intersection (map filter-map-keys maps)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(comment
+
+  (declare deep-merge)
+  (defn merge-entry [m e]
+    (let [k (key e)
+          v (val e)]
+      (if (and (contains? m k) (map? v))
+        (do
+          (println m)
+          (assoc m k (deep-merge (get m k))))
+        (do
+          (println "else")
+          (assoc m k v)))))
+
+  (defn deep-merge [maps]
+    ;(println maps)
+    (reduce (reduce merge-entry (first maps) (seq (second maps))) maps)))
+
+(comment
+  (defn deep-merge [maps]
+    (let [k-shared (key-intersection maps)]
+      (if (empty? k-shared)
+        (merge maps)
+        (deep-merge (merge-with merge maps))
+        ;(deep-merge (map #(map % maps) k-shared))
+        ))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/budget/core_test.clj
+++ test/budget/core_test.clj
@@ -2,8 +2,17 @@
   (:require [clojure.test :refer :all]
             [flipmunks.budget.core :refer :all]))
 
+
 (deftest a-test
   (testing "deep merge"
-    (is (= (deep-merge {:a 1} {:b 2}) {:a 1 :b 2}))
-    (is (= (deep-merge {:a 1} {:a 2}) {:a 2}))
-    (is (= (deep-merge {:a 1}) {:a 1}))))
+;    (is (= (deep-merge [{:a 1} {:b 2}]) {:a 1 :b 2}))
+; (is (= (deep-merge [{:a 1} {:a 2}]) {:a 2}))
+;    (is (= (deep-merge [{:a 1}]) {:a 1}))
+;(is (= (deep-merge [{:a 1 :b {:c 1}} {:b {:d 4}}]) {:a 1 :b {:c 1 :d 4}}))
+    ))
+
+(deftest keys-test
+  (testing "key intersection"
+    (is (= (key-intersection [{:a 1} {:b 2}]) #{}))
+    (is (= (key-intersection [{:a 1 :b 2} {:b 3}]) #{}))
+    (is (= (key-intersection [{:a 1 :b {:c 1}} {:b {:d 4}}]) #{:b}))))
