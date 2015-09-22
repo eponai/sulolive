17bb4c9b609ccbe1475dcd02bdd483436f1d5d65
Diana Gren <dianagren@gmail.com>
2015-09-22 12:37:50 +0200

    Remove and that I forgot.

--- src/server/flipmunks/budget/core.clj
+++ src/server/flipmunks/budget/core.clj
@@ -4,7 +4,7 @@
 
 (defn deep-merge [& maps]
   (let [merge-entry (fn [m [k v]]
-                      (if (and (map? v))                    ; might need to check if (get m k) is a map as well
+                      (if (map? v)                    ; might need to check if (get m k) is a map as well
                         (assoc m k (deep-merge (get m k) v))
                         (assoc m k v)))
         merge1 (fn [m1 m2]
