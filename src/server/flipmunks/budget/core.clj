240227637a6345c4386d2d799deda549694da273
Diana Gren <dianagren@gmail.com>
2015-09-22 11:51:47 +0200

    Remove now unused functions.

--- src/server/flipmunks/budget/core.clj
+++ src/server/flipmunks/budget/core.clj
@@ -2,24 +2,13 @@
   (:gen-class)
   (:require clojure.set))
 
-(defn filter-map-keys [m]
-  (->> m
-       (filter #(map? (second %)))
-       (map first)
-       (into #{})))
-
-(defn key-intersection [maps]
-  (->> maps
-       (map filter-map-keys)
-       (apply clojure.set/intersection)))
-
-(defn deep-merge [maps]
+(defn deep-merge [& maps]
   (let [merge-entry (fn [m me]
-              (let [k (key me)
-                    v (val me)]
-                (if (map? v)
-                  (assoc m k (deep-merge [(k m) v]))
-                  (assoc m k v))))
+                      (let [k (key me)
+                            v (val me)]
+                        (if (map? v)
+                          (assoc m k (deep-merge (k m) v))
+                          (assoc m k v))))
         merge1 (fn [m1 m2]
                  (reduce merge-entry m1 m2))]
     (reduce merge1 maps)))
--- test/budget/core_test.clj
+++ test/budget/core_test.clj
@@ -5,16 +5,10 @@
 
 (deftest a-test
   (testing "deep merge"
-    (is (= (deep-merge [{:a 1} {:b 2}]) {:a 1 :b 2}))
-    (is (= (deep-merge [{:a 1} {:a 2}]) {:a 2}))
-    (is (= (deep-merge [{:a 1}]) {:a 1}))
-    (is (= (deep-merge [{:a 1 :b {:c 1}} {:b {:d 4}}]) {:a 1 :b {:c 1 :d 4}}))
-    (is (= (deep-merge [{:a {:b {:c 2}}} {:a {:b {:d 3}}}]) {:a {:b {:c 2 :d 3}}}))
-    (is (= (deep-merge [{:a {:b {:c 2}}} {:a {:b {:d 3}}} {:a {:e 1 :b {:d 2}}}]) {:a {:b {:c 2 :d 2} :e 1}}))
+    (is (= (deep-merge  {:a 1} {:b 2}) {:a 1 :b 2}))
+    (is (= (deep-merge  {:a 1} {:a 2}) {:a 2}))
+    (is (= (deep-merge  {:a 1}) {:a 1}))
+    (is (= (deep-merge  {:a 1 :b {:c 1}} {:b {:d 4}}) {:a 1 :b {:c 1 :d 4}}))
+    (is (= (deep-merge  {:a {:b {:c 2}}} {:a {:b {:d 3}}}) {:a {:b {:c 2 :d 3}}}))
+    (is (= (deep-merge  {:a {:b {:c 2}}} {:a {:b {:d 3}}} {:a {:e 1 :b {:d 2}}}) {:a {:b {:c 2 :d 2} :e 1}}))
     ))
-
-(deftest keys-test
-  (testing "key intersection"
-    (is (= (key-intersection [{:a 1} {:b 2}]) #{}))
-    (is (= (key-intersection [{:a 1 :b 2} {:b 3}]) #{}))
-    (is (= (key-intersection [{:a 1 :b {:c 1}} {:b {:d 4}}]) #{:b}))))
