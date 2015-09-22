fd74aaf35ff3f2aa95ff5e3c58899881026d6e1b
Diana Gren <dianagren@gmail.com>
2015-09-22 11:35:17 +0200

    Working deep-merge.

--- src/server/flipmunks/budget/core.clj
+++ src/server/flipmunks/budget/core.clj
@@ -3,50 +3,27 @@
   (:require clojure.set))
 
 (defn filter-map-keys [m]
-  (into #{} (map first (filter #(map? (second %)) m))))
+  (->> m
+       (filter #(map? (second %)))
+       (map first)
+       (into #{})))
 
 (defn key-intersection [maps]
-  (apply clojure.set/intersection (map filter-map-keys maps)))
+  (->> maps
+       (map filter-map-keys)
+       (apply clojure.set/intersection)))
 
+(defn deep-merge [maps]
+  (let [map-a (first maps)
+        map-b (second maps)
+        red (fn [m me]
+              (let [k (key me)
+                    v (val me)]
+                (if (map? v)
+                  (assoc m k (deep-merge [(k m) v]))
+                  (assoc m k v))))]
+    (reduce red map-a map-b)))
 
-
-
-
-
-
-
-
-
-
-
-
-
-(comment
-
-  (declare deep-merge)
-  (defn merge-entry [m e]
-    (let [k (key e)
-          v (val e)]
-      (if (and (contains? m k) (map? v))
-        (do
-          (println m)
-          (assoc m k (deep-merge (get m k))))
-        (do
-          (println "else")
-          (assoc m k v)))))
-
-  (defn deep-merge [maps]
-    ;(println maps)
-    (reduce (reduce merge-entry (first maps) (seq (second maps))) maps)))
-
-(comment
-  (defn deep-merge [maps]
-    (let [k-shared (key-intersection maps)]
-      (if (empty? k-shared)
-        (merge maps)
-        (deep-merge (merge-with merge maps))
-        ;(deep-merge (map #(map % maps) k-shared))
-        ))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/budget/core_test.clj
+++ test/budget/core_test.clj
@@ -5,10 +5,11 @@
 
 (deftest a-test
   (testing "deep merge"
-;    (is (= (deep-merge [{:a 1} {:b 2}]) {:a 1 :b 2}))
-; (is (= (deep-merge [{:a 1} {:a 2}]) {:a 2}))
-;    (is (= (deep-merge [{:a 1}]) {:a 1}))
-;(is (= (deep-merge [{:a 1 :b {:c 1}} {:b {:d 4}}]) {:a 1 :b {:c 1 :d 4}}))
+    (is (= (deep-merge [{:a 1} {:b 2}]) {:a 1 :b 2}))
+    (is (= (deep-merge [{:a 1} {:a 2}]) {:a 2}))
+    (is (= (deep-merge [{:a 1}]) {:a 1}))
+    (is (= (deep-merge [{:a 1 :b {:c 1}} {:b {:d 4}}]) {:a 1 :b {:c 1 :d 4}}))
+    (is (= (deep-merge [{:a {:b {:c 2}}} {:a {:b {:d 3}}}]) {:a {:b {:c 2 :d 3}}}))
     ))
 
 (deftest keys-test
