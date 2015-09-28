b13351c6a6820aa8dd7f8b3c5ea1625d745800be
Petter Eriksson <periksson@splunk.com>
2015-09-28 17:26:41 +0700

    Trying things with no-local-state

--- src/client/flipmunks/budget/omtest.cljs
+++ src/client/flipmunks/budget/omtest.cljs
@@ -11,6 +11,10 @@
                                                              :price 600}}]
                            :rates {"LEK" 0.0081}}}}})
 
+(defn days-in-month [x]
+  ;;TODO: GET A DATE LIBRARY
+  31)
+
 (defn month-to-str [n]
   ;;TODO: GET A DATE LIBRARY
   (get {1 "January"
@@ -24,29 +28,42 @@
    [:div {:class "col-md-6"} name]
    [:div {:class "col-md-6"} (str (:price cost) " " (:currency cost))]])
 
-(defn day-budget [[day {:keys [purchases rates]}]]
-  (prn purchases)
-  (let [prices (map (juxt (comp :price :cost)
-                          #(let [curr (-> % :cost :currency)]
-                             (* (get rates curr)
-                                (-> % :cost :price))))
-                    purchases)]
-    (print prices)
-    ;;TODO: want to be able to expand this view. What about local state?
-    (html
-      [:div {:class "row"}
-       [:div {:class "col-md-4"} (str "Day " day ".")]
-       [:div {:class "col-md-4"} (reduce + 0 (map first prices))]
-       [:div {:class "col-md-4"} (reduce + 0 (map second prices))]
-       (map (partial purchase-view rates) purchases)])))
+(defn day-budget-view [day budget-day owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:expanded false})
+    om/IRenderState
+    (render-state [_ {:keys [expanded]}]
+      (let [{:keys [purchases rates]} @budget-day
+            prices (map (juxt (comp :price :cost)
+                              #(let [curr (-> % :cost :currency)]
+                                 (* (get rates curr)
+                                    (-> % :cost :price))))
+                        purchases)]
+        ;;TODO: want to be able to expand this view. What about local state?
+        (html
+          [:div {:class "row"
+                 :on-click #(om/update-state! owner :expanded not)} ;;inverse the value
+           "day: " day
+           (when expanded
+             [:div {:class "col-md-4"} (str "Day " day ".")] 
+             [:div {:class "col-md-4"} (reduce + 0 (map first prices))] 
+             [:div {:class "col-md-4"} (reduce + 0 (map second prices))] 
+             (map (partial purchase-view rates) purchases))])))))
 
-(defn month-budget-view [month-data owner]
+(defn month-budget-view [month days owner]
   (reify
     om/IRender
     (render [this]
-      (html [:div (map day-budget (into (sorted-map) @month-data))]))))
+      (html [:div 
+             (for [day (range (days-in-month month))
+                   :when (contains? @days day)]
+               (do
+                 (om/build (partial day-budget-view day) (get days day))))]))))
 
 (defn widget [data owner]
+  (prn {:widget-render-state @data})
   (reify
     om/IRender
     (render [this]
@@ -55,7 +72,7 @@
                [:div {:class "page-header"}
                 [:h1 "Budget App"]
                 [:h2 (str year "-" (month-to-str month))]]
-               (om/build month-budget-view 
+               (om/build (partial month-budget-view month)
                          (-> data :budget-data (get year) (get month)))])))))
 
 
@@ -64,5 +81,10 @@
                          :budget-data testdata})]
     (om/root widget 
              app-state
-             {:target (. js/document (getElementById "my-app"))})))
+             {:target (. js/document (getElementById "my-app"))
+              :instrument (let [desc (om/no-local-descriptor om/no-local-state-methods)]
+                           (fn [f cursor m]
+                            (om/build* f cursor 
+                                       (assoc m :descriptor desc))))
+              })))
 
