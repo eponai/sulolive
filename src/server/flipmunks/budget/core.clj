cd9d612235fb0d2bd8b87cc45435771b9bd73d5c
Diana Gren <dianagren@gmail.com>
2015-10-07 17:10:28 +0700

    Inline date and currency creation, will merge if already exists.

--- src/server/flipmunks/budget/core.clj
+++ src/server/flipmunks/budget/core.clj
@@ -9,109 +9,97 @@
             [clj-time.core :as t]
             [clj-time.format :as f]
             [clj-time.coerce :as c]
-            [datomic.api :only [q db] :as d]))
-
-(def test-data (atom {:currency :USD
-                      :dates    {2015 {1 {3 {:purchases [{:name "coffee" :cost {:currency :LEK :price 150}}]
-                                             :rates     {:LEK 0.0081
-                                                         :SEK 0.1213}}
-                                          4 {:purchases [{:name "coffee" :cost {:currency :LEK :price 150}}, {:name "market" :cost {:currency :LEK :price 300}}]
-                                             :rates     {:LEK 0.0081
-                                                         :SEK 0.1213}}}}}}))
-(defn deep-merge
-  "Returns a map that consists of the rest of the maps merged onto the first.\n
-  If a key occurs in more than one map, the mapping from\n  the latter (left-to-right) will be the mapping in the result.\n
-  If a key holding a map value occurs in more than one map, the mapping(s) from the latter (left-to-right)\n
-  will be combined with the mapping in the result by calling (deep-merge val-in-result val-in-latter)."
-  [& maps]
-  (when (some identity maps)
-    (let [merge-entry (fn [m [k v]]
-                        (if (and (map? v) (map? (get m k)))
-                          (assoc m k (deep-merge (get m k) v))
-                          (assoc m k v)))
-          merge1 (fn [m1 m2]
-                   (reduce merge-entry m1 m2))]
-      (reduce merge1 maps))))
+            [datomic.api :only [q db] :as d])
+  (:import (java.util UUID)))
 
 (defroutes app-routes
            (context "/entries" [] (defroutes entries-routes
-                                             (GET "/" [] (str @test-data))
-                                             (POST "/" {body :body} (str (swap! test-data deep-merge body)))))
+                                             (GET "/" [] "")
+                                             (POST "/" {body :body} "")))
            (route/not-found "Not Found"))
 
 (def app
    (middleware/wrap-json-response (middleware/wrap-json-body (handler/api app-routes) {:keywords? true})))
 
-(defn key-prefix [k p]
+(defn key-prefix
+  "Returns a new keyword with k prefixed with p. E.g. (key-prefix :k 'p/') will return :p/k."
+  [k p]
   (keyword (str p (name k))))
 
-(defn user-tx->db-tx [user-tx]
-  (let [conv-fn {:currency (fn [v] [:currency/code v])
-                 :date     (fn [d] [:date/ymd d])
-                 :tags     (fn [tags] (mapv (fn [n] {:db/id (d/tempid :db.part/user)
-                                                     :tag/name n
-                                                     :tag/persistent false}) tags))
-                 :amount   (fn [a] (bigint a))}
-        map-fn (fn [[k v]]
-             [(key-prefix k "transaction/") ((or (k conv-fn) identity) v)])]
-    (assoc (into {} (map map-fn user-tx)) :db/id (d/tempid :db.part/user))))
+(defn trans
+  "Transform data by applying key-fn to all the keys in the map, and using a map for applying different\n
+   functions to the values for the given keys in value-fn-map. If no function is specified for a certain key,\n
+   that value will be unchanged."
+  [data key-fn value-fn-map]
+  (let [map-fn (fn [[k v]]
+                 [(key-fn k) ((or (k value-fn-map) identity) v)])]
+    (into {} (map map-fn data))))
 
 ;; Handle fetch currency data and insert in datomic.
 (def currencies
-  (json/read-str (:body (client/get "https://openexchangerates.org/api/currencies.json?app_id=APP_ID")) :key-fn keyword))
+  ;(json/read-str (:body (client/get "https://openexchangerates.org/api/currencies.json?app_id=APP_ID")) :key-fn keyword)
+  )
 
 (def currency-rate-data
-  (json/read-str (:body (client/get "https://openexchangerates.org/api/latest.json?app_id=APP_ID")) :key-fn keyword))
+  ;(json/read-str (:body (client/get "https://openexchangerates.org/api/latest.json?app_id=APP_ID")) :key-fn keyword)
+  )
 
-(defn db-enum
-  "Returns a datomic currency enum for the given keyword.\n
-  Example: code-key :SEK returns :currency/SEK."
-  [prefix code-key]
-  (keyword (str prefix (name code-key))))
-
-(defn db-enums
-  "Returns vector of datomic entities representing enums using the data from the specified map m.\n
-  Key k in m are represent the db/ident value with the applied enum-fn, (enum-fn k) will be called where enum-fn should take a keyword as argument.\n
-  The values in m are the values for entity attribute attr."
-  [m]
-  (let [map-fn (fn [[k v]]
-                 {:db/id         (d/tempid :db.part/user)
-                  :currency/code k
-                  :currency/name v})]
-    (mapv map-fn m)))
-
-(defn ymd-string
+(defn date->ymd-string
   "Takes a date and returns a string for that date of the form yyy-MM-dd."
   [d]
   (f/unparse-local (f/formatters :date) d))
 
-(defn db-date
-  "Returns a map representing a date datomic entity."
+(defn date->db-tx
+  "Returns a map representing a date datomic entity for the specified LocalDateTime."
   [date]
   {:db/id          (d/tempid :db.part/user)
-   :date/ymd       (ymd-string date)
+   :date/ymd       (date->ymd-string date)
    :date/year      (t/year date)
    :date/month     (t/month date)
    :date/day       (t/day date)
    :date/timestamp (c/to-long date)})
 
-(defn db-currency-rates
+(defn date-str->db-tx
+  "Returns a LocalDateTime for the string of format 'yyy-MM-dd'."
+  [date-str]
+  (date->db-tx (f/parse-local (f/formatters :date) date-str)))
+
+(defn tags->db-tx
+  "Returns datomic entities representing tags, given a vector of tag names."
+  [tags]
+  (mapv (fn [n] {:db/id          (d/tempid :db.part/user)
+                 :tag/name       n
+                 :tag/persistent false}) tags))
+
+(defn user-tx->db-tx
+  "Takes a user input transaction and converts into a datomic entity.\n
+  A conversion function will be applied on the values for the following keys #{:currency :date :tags :amount :uuid}\n
+  as appropriate for the database. All keys will be prefixed with 'transaction/' (e.g. :name -> :transaction/name)"
+  [user-tx]
+  (let [conv-fn-map {:currency (fn [c] {:db/id         (d/tempid :db.part/user)
+                                        :currency/code c})
+                     :date     (fn [d] (date-str->db-tx d))
+                     :tags     (fn [tags] (tags->db-tx tags))
+                     :amount   (fn [a] (bigint a))
+                     :uuid     (fn [uuid] (java.util.UUID/fromString uuid))}]
+    (assoc (trans user-tx #(key-prefix % "transaction/") conv-fn-map) :db/id (d/tempid :db.part/user))))
+
+(defn cur-rates->db-txs
   "Returns a vector with datomic entites representing a currency conversions for the given date.\n
   d a LocalDate to be matched to the conversion.\n
   m map with timestamp and rates of the form {:timestamp 190293 :rates {:SEK 8.333 ...}}
   "
-  [d m]
+  [date-ymd m]
   (let [timestamp (:timestamp m)
-        date-ymd (ymd-string d)
         map-fn (fn [[code rate]]
                  {:db/id                (d/tempid :db.part/user)
                   :conversion/timestamp timestamp
-                  :conversion/date      [:date/ymd date-ymd]
+                  :conversion/date      (date-str->db-tx date-ymd)
                   :conversion/currency  [:currency/code code]
                   :conversion/rate      (bigdec rate)})]
     (mapv map-fn (:rates m))))
 
 (defn -main [& args]
-  (println (db-date (t/today)))
+  ;(println (db-date (t/today)))
   ;(println (db-enums currencies currency-enum :currency/name))
   )
