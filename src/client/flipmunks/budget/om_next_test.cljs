e958edabe72179ba7a21d3e431715a48d759ec78
Petter Eriksson <periksson@splunk.com>
2015-10-09 14:29:22 +0700

    Sablono and om-next

--- src/client/flipmunks/budget/om_next_test.cljs
+++ src/client/flipmunks/budget/om_next_test.cljs
@@ -1,15 +1,20 @@
-(ns flipmunks.budget.om-next-test
+(ns flipmunks.budget.om_next_test
   (:require [goog.dom :as gdom]
+            [sablono.core :as html :refer-macros [html]]
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
 (defui HelloWorld
   Object
   (render [this]
-    (dom/div nil "Hello World")))
+    (html [:div (get (om/props this) :title)])))
 
 (def hello (om/factory HelloWorld))
 
 (defn run []
-  (js/React.render (hello) (gdom/getElement "my-app")))
+  (js/React.render 
+    (html [:p 
+           (map #(hello {:title (str "Hello " %)})
+                (range 3))]) 
+    (gdom/getElement "my-app")))
 
