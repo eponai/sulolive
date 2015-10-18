55ab2e4faf33ae7f3be987e094a511106126df93
Petter Eriksson <periksson@splunk.com>
2015-10-18 16:42:31 +0700

    Viewing ui.transactions in many states

--- src/client/flipmunks/budget/ui/transactions.cljs
+++ src/client/flipmunks/budget/ui/transactions.cljs
@@ -18,50 +18,60 @@
          3 "rd"
          "th")))
 
+(defn style [style-map]
+  {:style (clj->js style-map)})
+
+(defn render-tag [tag-name]
+  (vector :span (style {:padding "0.5em" :border "2px"}) 
+          tag-name))
+
 (defui Transaction
   static om/IQuery
   (query [this] [:db/id :transaction/uuid
                  :transaction/name :transaction/amount
+                 :transaction/details
                  {:transaction/date [:date/ymd :date/day]}
                  {:transaction/currency [:currency/name]}
                  {:transaction/tags [:tag/name]}
 
                  :ui.transaction/expanded
                  :ui.transaction/edit-mode
+                 :ui.transaction/show-tags
                  :ui.transaction/expanded])
     Object
     (render [this]
             (let [{:keys [transaction/tags
                           transaction/date
                           transaction/amount
-                          transaction/currency]} (om/props this)
-                  ]
+                          transaction/currency
+                          transaction/details
+                          ui.transaction/show-tags]} (om/props this)]
               (prn tags)
               (html
                 [:div 
-                 ;; TODO: Figure out what to do about the #
-                 [:style (str "#"(css [:foo:active {:background-color "#ddd"}]))]
-                [:div {:id "foo"
-                        :style #js {:display "flex"
-                                    :flex-direction "column"}}
-                     [:div {:style #js {:display "flex"
-                                        :flex-direction "row"
-                                        :flex-wrap "nowrap"
-                                        :justify-content "space-between"}}
-                      [:div {:style #js {:display "flex"
-                                         :flex-direction "row"}}
-                       (day-of-the-week date)]
-                      [:div {:style #js {:display "flex"
-                                         :flex-direction "reverse-row"}}
-                     (str amount " " (:currency/name currency))]]
-                     [:div {:style #js {:display "flex" :flex-direction "column"
-                                        :align-items "center"}}
-                      [:div {:style #js {:height "1px" :width "50%" :background-color "#ddd"}}]]
-                     [:div "Tags: "
-                      (->> tags
-                           (map :tag/name)
-                           (map #(vector :span {:style #js {:padding "0.3em"
-                                                            :border "2px"}} %)))]]]))))
+                 [:style (css [:#day:active {:background-color "rgb(250,250,250)"}])]
+                 [:div (assoc (style {:display "flex" :flex-direction "column"
+                                      :borderStyle "solid"
+                                      :borderWidth "1px"
+                                      :borderRadius "0.5em"
+                                      :padding "0.5em"}) 
+                              :id "day")
+                  [:div (style {:display "flex"
+                                :flex-direction "row"
+                                :flex-wrap "nowrap"
+                                :justify-content "space-between"})
+                   [:p (style {:display "flex"
+                                 :flex-direction "row"
+                                 :fontWeight "bold"})
+                    (day-of-the-week date)]
+                   [:div (style {:display "flex"
+                                 :flex-direction "reverse-row"})
+                    (str amount " " (:currency/name currency))]]
+                  (when details
+                     [:div details])
+                  (when show-tags
+                    [:div
+                     (->> tags (map :tag/name) (map render-tag))])]]))))
 
 (def transaction (om/factory Transaction))
 
@@ -73,9 +83,28 @@
    :transaction/currency {:currency/name currency}
    :transaction/tags (mapv #(hash-map :tag/name %) tags)})
 
+(def standard-transaction 
+  (transaction-props "coffee" [2015 10 16] 140 "THB" 
+                     ["thailand" "2015" "chiang mai"]))
+
 (defcard transaction-card
-  (let [data (transaction-props "coffee" [2015 10 16] 140 "THB" 
-                                ["thailand" "2015" "chiang mai"])]
-    (prn data)
-    (transaction data)))
+  (transaction standard-transaction))
+
+(defcard transaction-with-details
+  (transaction (assoc standard-transaction
+                      :transaction/details
+                      "Great latte next to the dumpling place. We should come back here next time we're in Chaing Mai, Thailand.")))
+
+(defcard transaction-with-tags
+  (transaction (assoc standard-transaction
+                      :ui.transaction/show-tags
+                      true)))
+
+(defcard transaction-with-details-and-tags
+  (transaction (assoc standard-transaction
+                      :transaction/details
+                      "Very good latte! We should come back."
+                      :ui.transaction/show-tags
+                      true)))
+
 
