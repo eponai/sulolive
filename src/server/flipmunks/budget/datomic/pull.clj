ecfc908a5d81eda27e4bb2cfc74ea3680e614b4f
Diana Gren <dianagren@gmail.com>
2015-10-19 14:45:54 +0700

    Minor cleanup.

--- src/server/flipmunks/budget/datomic/pull.clj
+++ src/server/flipmunks/budget/datomic/pull.clj
@@ -11,40 +11,41 @@
                                        :exception e})))))
 
 ; Pull and format datomic entries
-(defn- distinct-ids
-  "Get distinct db ids from the nested entity with the given keyword k in a list of entities."
-  ([entities attr]
-   (mapv :db/id (set (flatten (map attr entities))))))
-
-(defn- distinct-attrs [ents]
-  (->> ents
-       (map keys)
+(defn- distinct-values
+  "Map function f over the given collection, and return a set of the distinct flattened values."
+  [coll f]
+  (->> coll
+       (map f)
        flatten
        set))
-
-(defn- db-entities
-  "Get vector of entity maps for given entity ids."
-  [db ids]
-  (mapv #(into {:db/id %} (d/entity db %)) ids))
 
 (defn- tx-query
   "Create the query for pulling transactions given some date parameters."
   [params]
   (let [query '[:find [(pull ?t [*]) ...]
                 :in $ ?email
-                :where [?u :user/email ?email] [?u :user/transactions ?t] [?t :transaction/date ?d]]
+                :where [?u :user/email ?email]
+                [?u :user/transactions ?t]
+                [?t :transaction/date ?d]]
         key-map {:y :date/year
                  :m :date/month
                  :d :date/day}
         map-fn (fn [[k v]] ['?d (k key-map) (Long/parseLong v)])]
-    (apply conj query (map map-fn (select-keys params (keys key-map))))))
+    (->> key-map
+         keys
+         (select-keys params)
+         (map map-fn)
+         (apply conj query))))
 
 (defn user-txs [db user-email params]
   (when user-email
     (q (tx-query params) db user-email)))
 
-(defn- nested-entities [db user-txs attr]
-  (db-entities db (distinct-ids user-txs attr)))
+(defn- db-entities [db user-txs attr]
+  (let [distinct-ids (map :db/id (distinct-values
+                                   user-txs
+                                   attr))]
+    (map #(into {:db/id %} (d/entity db %)) distinct-ids)))
 
 (defn conversions [db user-tx-ids]
   (q '[:find [(pull ?c [*]) ...]
@@ -58,7 +59,7 @@
 
 (defn all-data [db user-email params]
   (let [user-txs (user-txs db user-email params)
-        entities (partial nested-entities db user-txs)]
+        entities (partial db-entities db user-txs)]
     (vec (concat user-txs
                  (entities :transaction/date)
                  (entities :transaction/currency)
@@ -71,7 +72,7 @@
   ([db data]
    (schema db identity data))
   ([db f data]
-   (filter f (map #(into {} (d/entity db %)) (distinct-attrs data)))))
+   (filter f (map #(into {} (d/entity db %)) (distinct-values data keys)))))
 
 (defn currencies [db]
   (q '[:find [(pull ?e [*]) ...]
