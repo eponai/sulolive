5911263f80963bb556a144f9b62aa969ae4ca195
Petter Eriksson <ipetterik@gmail.com>
2015-10-30 14:54:54 +0100

    New layout

--- src/client/flipmunks/budget/ui/transactions.cljs
+++ src/client/flipmunks/budget/ui/transactions.cljs
@@ -26,81 +26,77 @@
 
 (defn render-tag [tag-name]
   [:div (style {:display "inline-block"})
-   [:style (css [:#ui-transaction-tag {:display "inline-block"
-                                       :padding "0.2em 0.2em" 
-                                       :margin "0.2em"
-                                       :border-width "1px"
-                                       :border-style "solid"
-                                       :border-radius "0.3em"
+   [:style (css [:#ui-transaction-tag {:display        "inline-block"
+                                       :padding        "0.2em 0.2em"
+                                       :margin         "0.2em"
+                                       :border-width   "1px"
+                                       :border-style   "solid"
+                                       :border-radius  "0.3em"
                                        :text-transform "capitalize"
-                                       :font-size "1em"
-                                       :border-color "#ddd"
-                                       :cursor "default"}
+                                       :font-size      "1em"
+                                       :border-color   "#ddd"
+                                       :cursor         "default"}
                  [:&:hover {:border-color "#aaa"}]
                  [:&:active {:border-color "#ddd"}]])]
-   [:div {:id "ui-transaction-tag"
+   [:div {:id       "ui-transaction-tag"
           :on-click #(prn %)}
     tag-name]])
 
 (defui Transaction
-  static om/IQuery
-  (query [this] [:db/id :transaction/uuid
-                 :transaction/name :transaction/amount
-                 :transaction/details
-                 {:transaction/date [:date/ymd :date/day]}
-                 {:transaction/currency [:currency/name]}
-                 {:transaction/tags [:tag/name]}
+       static om/IQuery
+       (query [this] [:db/id :transaction/uuid
+                      :transaction/name :transaction/amount
+                      :transaction/details
+                      {:transaction/date [:date/ymd :date/day]}
+                      {:transaction/currency [:currency/name]}
+                      {:transaction/tags [:tag/name]}
 
-                 :ui.transaction/expanded
-                 :ui.transaction/edit-mode
-                 :ui.transaction/show-tags
-                 :ui.transaction/expanded])
-  Object
-  (render [this]
-          (let [{;; rename to avoid replacing clojure.core/name
-                 transaction-name :transaction/name 
-                 :keys [transaction/tags
-                        transaction/date
-                        transaction/amount
-                        transaction/currency
-                        transaction/details
-                        ui.transaction/show-tags]} (om/props this)]
-            (prn tags)
-            (html
-              [:div 
-               [:style (css [:#ui-transaction {:background-color #"fff"}
-                             [:&:hover {:background-color "rgb(250,250,250)"}]
-                             [:&:active {:background-color "#eee"}]])]
-               [:div (-> (style {:display "flex" :flex-direction "column"
-                                 :border-style "solid"
-                                 :border-width "0px 1px 1px 1px"
-                                 :border-radius "0.5em"
-                                 :padding "0.5em"}) 
-                         (assoc :id "ui-transaction"))
-                [:div (style {:display "flex"
-                              :flex-direction "row"
-                              :flex-wrap "nowrap"
-                              :align-items "center"
-                              :justify-content "space-between"})
-                 [:div (style {:display "flex"
-                               :flex-direction "row"})
-                  transaction-name]
-                 [:div (style {:display "flex"
-                               :flex-direction "reverse-row"})
-                  (str amount " " (:currency/name currency))]]
-                (when details
-                  [:div (style {:margin "0em 1.0em":padding "0.3em"
-                                :fontStyle "italic"}) 
-                   details])
-                (when show-tags
-                  [:div
-                   (->> tags (map :tag/name) (map render-tag))])]]))))
+                      :ui.transaction/expanded
+                      :ui.transaction/edit-mode
+                      :ui.transaction/show-tags
+                      :ui.transaction/expanded])
+       Object
+       (render [this]
+               (let [{;; rename to avoid replacing clojure.core/name
+                      transaction-name :transaction/name
+                      :keys            [transaction/tags
+                                        transaction/date
+                                        transaction/amount
+                                        transaction/currency
+                                        transaction/details
+                                        ui.transaction/show-tags]} (om/props this)]
+                 (html
+                   [:div
+                    [:style (css [:#ui-transaction {:background-color #"fff"}
+                                  [:&:hover {:background-color "rgb(250,250,250)"}]
+                                  [:&:active {:background-color "#eee"}]])]
+                    [:div (-> (style {:display        "flex"
+                                      :flex-direction "column"
+                                      :border-style "solid"
+                                      :border-width "1px 0px 0px"
+                                      :border-color "#ddd"
+                                      :padding        "0.5em"})
+                              (assoc :id "ui-transaction"))
+                     [:div (style {:display         "flex"
+                                   :flex-direction  "row"
+                                   :justify-content "flex-start"
+                                   })
+                      [:div (str amount " " (:currency/name currency))]
+                      [:div (style {:margin-left "0.5em"}) transaction-name]]
+                     (when details
+                       [:div (style {:margin    "0em 1.0em"
+                                     :padding   "0.3em"
+                                     :fontStyle "italic"})
+                        details])
+                     (when show-tags
+                       [:div
+                        (->> tags (map :tag/name) (map render-tag))])]]))))
 
 (def transaction (om/factory Transaction))
 
 ;; Transactions grouped by a day
 
-(defn day-of-the-week 
+(defn day-of-the-week
   "Given date, returns name of the date and it's number with the appropriate suffix.
   Examples:
   2015-10-16 => Friday 16th
@@ -117,63 +113,57 @@
   "Adds transactions amounts together.
   TODO: Needs to convert the transactions to the 'primary currency' some how."
   [transactions]
-  {:amount (transduce (map :transaction/amount) + 0 transactions)
+  {:amount   (transduce (map :transaction/amount) + 0 transactions)
    ;; TODO: Should instead use the primary currency
    :currency (-> transactions first :transaction/currency :currency/name)})
 
 (defn expand-day [this {:keys [db/id ui.day/expand]}]
-  (om/transact! this 
+  (om/transact! this
                 `[(datascript/transact
-                    [[:db.fn/cas ~id 
-                      :ui.day/expand 
+                    [[:db.fn/cas ~id
+                      :ui.day/expand
                       ~expand
                       (not ~expand)]])]))
 
 (defui DayTransactions
-  static om/IQueryParams
-  (params [this] {:transactions (om/get-query Transaction)})
-  static om/IQuery
-  (query [this] 
-         '[:db/id
-           :date/year
-           :date/month
-           :date/day
-           :transaction/_date ?transactions
+       static om/IQueryParams
+       (params [this] {:transactions (om/get-query Transaction)})
+       static om/IQuery
+       (query [this]
+              '[:db/id
+                :date/year
+                :date/month
+                :date/day
+                :transaction/_date ?transactions
 
-           :ui.day/expanded])
-  Object
-  (render [this]
-          (let [{transactions :transaction/_date
-                 :keys [date/year date/month date/day date/ymd
-                        ui.day/expanded] :as props} (om/props this)]
-            (html [:div 
-                   [:style (css [:#ui-day {:background-color "#fff"}
-                                 [:&:hover {:background-color "rgb(250,250,250)"}]
-                                 [:&:active {:background-color "#eee"}]])]
-                   [:div (-> (style {:borderWidth (if expanded "1px" "1px 1px 3px 1px")
-                                     :borderStyle "solid"
-                                     :borderRadius "0.5em"
-                                     :padding "0.5em"})
-                             (assoc :id "ui-day"
-                                    :on-click #(expand-day this props)))
-                    [:div (style {:display "flex"
-                                  :flex-direction "row"
-                                  :flex-wrap "nowrap"
-                                  :align-items "center"
-                                  :justify-content "space-between"})
-                     [:div (style {:display "flex"
-                                   :flex-direction "row"
-                                   :fontSize "1.3em"
-                                   :fontWeight "bold"})
-                      [:span (day-of-the-week props)]]
-                     [:div (style {:display "flex"
-                                   :flex-direction "reverse-row"
-                                   :fontSize "1.3em"
-                                   :fontWeight "bold"})
-                      (let [{:keys [amount currency]} (sum transactions)]
-                        (str amount " " currency))]]]
-                   (when expanded
-                     [:div 
-                      (map transaction transactions)])]))))
+                :ui.day/expanded])
+       Object
+       (render [this]
+               (let [{transactions :transaction/_date
+                      :keys        [ui.day/expanded] :as props} (om/props this)]
+                 (html [:div
+                        (style {:borderWidth "0px 0px 1px"
+                                :borderStyle "solid"})
+                        [:style (css [:#ui-day {:background-color "#fff"}
+                                      [:&:hover {:background-color "rgb(250,250,250)"}]
+                                      [:&:active {:background-color "#eee"}]])]
+                        [:div (-> (style {:display         "flex"
+                                          :flex-direction  "row"
+                                          :flex-wrap       "nowrap"
+                                          :align-items     "center"
+                                          :justify-content "flex-start"})
+                                  (assoc :id "ui-day"
+                                         :on-click #(expand-day this props)))
+                         [:div (style {:fontSize   "1.3em"
+                                       :fontWeight "bold"})
+                          (let [{:keys [amount currency]} (sum transactions)]
+                            (str amount " " currency))]
+                         [:div (style {:fontSize    "1.3em"
+                                       :fontWeight  "bold"
+                                       :margin-left "0.5em"})
+                          [:span (day-of-the-week props)]]]
+                        (when expanded
+                          [:div
+                           (map transaction transactions)])]))))
 
 (def day-of-transactions (om/factory DayTransactions))
--- test/devcards/flipmunks/budget/ui/transactions_dc.cljs
+++ test/devcards/flipmunks/budget/ui/transactions_dc.cljs
@@ -1,56 +1,61 @@
 (ns flipmunks.budget.ui.transactions_dc
   (:require [devcards.core :as dc]
+            [sablono.core :refer-macros [html]]
             [flipmunks.budget.ui.transactions :as t])
-  (:require-macros [devcards.core :refer [defcard]])) 
+  (:require-macros [devcards.core :refer [defcard]]))
 
 (defn transaction-props* [name [y m d] amount currency tags]
-  {:transaction/name name
-   :transaction/amount amount
+  {:transaction/name     name
+   :transaction/amount   amount
    :transaction/currency {:currency/name currency}
-   :transaction/tags (mapv #(hash-map :tag/name %) tags)
-   :transaction/date {:date/ymd (str y "-" m "-" d)
-                      :date/year y :date/month m :date/day d}})
+   :transaction/tags     (mapv #(hash-map :tag/name %) tags)
+   :transaction/date     {:date/ymd  (str y "-" m "-" d)
+                          :date/year y :date/month m :date/day d}})
 
-(def transaction-props 
-  (transaction-props* "Coffee" [2015 10 16] 140 "THB" 
-                     ["thailand" "2015" "chiang mai"]))
+(def transaction-props
+  (transaction-props* "Coffee" [2015 10 16] 140 "THB"
+                      ["thailand" "2015" "chiang mai"]))
 
 (defcard transaction-card
-  (t/transaction transaction-props))
+         (t/transaction transaction-props))
 
 (defcard transaction-with-details
-  (t/transaction (assoc transaction-props
-                        :transaction/details
-                        "Great latte next to the dumpling place. We should come back here next time we're in Chaing Mai, Thailand.")))
+         (t/transaction (assoc transaction-props
+                          :transaction/details
+                          "Great latte next to the dumpling place. We should come back here next time we're in Chaing Mai, Thailand.")))
 
 (defcard transaction-with-tags
-  (t/transaction (assoc transaction-props
-                        :ui.transaction/show-tags
-                        true)))
+         (t/transaction (assoc transaction-props
+                          :ui.transaction/show-tags
+                          true)))
 
 (def t-with-details-and-tags
   (assoc transaction-props
-         :transaction/details
-         "Very good latte! We should come back."
-         :ui.transaction/show-tags
-         true))
+    :transaction/details
+    "Very good latte! We should come back."
+    :ui.transaction/show-tags
+    true))
 
 (defcard transaction-with-details-and-tags
-  (t/transaction t-with-details-and-tags))
+         (t/transaction t-with-details-and-tags))
 
 (def day-props (assoc (:transaction/date transaction-props)
-                      :transaction/_date [transaction-props]))
+                 :transaction/_date [transaction-props]))
 
 (defcard day-of-transactions
-  (t/day-of-transactions day-props))
+         (t/day-of-transactions day-props))
 
 (defcard day-of-transactions-expanded
-  (t/day-of-transactions (assoc day-props
-                                :ui.day/expanded
-                                true)))
+         (t/day-of-transactions (assoc day-props
+                                  :ui.day/expanded
+                                  true)))
 
-(defcard day-of-transactions-expanded--2-transactions
-  (t/day-of-transactions (-> day-props 
-                             (assoc :ui.day/expanded true)
-                             (update :transaction/_date conj t-with-details-and-tags))))
-
+(let [expanded-trans (-> day-props
+                         (assoc :ui.day/expanded true)
+                         (update :transaction/_date conj t-with-details-and-tags))]
+  (defcard day-of-transactions-expanded--2-transactions
+           (t/day-of-transactions expanded-trans))
+  (defcard two-days-of-transactions
+           (html [:div
+                  (t/day-of-transactions expanded-trans)
+                  (t/day-of-transactions day-props)])))
