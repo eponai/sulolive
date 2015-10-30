5441d079b4153c8acf4a4af8bca2a4868a9702f2
Petter Eriksson <ipetterik@gmail.com>
2015-10-30 16:21:27 +0100

    ui header devcard skeleton

---
+++ src/client/flipmunks/budget/ui/header.cljs
@@ -0,0 +1,4 @@
+(ns flipmunks.budget.ui.header
+  (:require [om.next :as om :refer-macros [defui]]
+            [sablono.core :as html :refer-macros [html]]
+            [garden.core :refer [css]]))
--- test/devcards/flipmunks/budget/devcards_main.cljs
+++ test/devcards/flipmunks/budget/devcards_main.cljs
@@ -3,6 +3,7 @@
     [devcards.core :as dc]
     [sablono.core :as html :refer-macros [html]]
     [flipmunks.budget.ui.new_transaction_dc]
+    [flipmunks.budget.ui.header_dc]
     [flipmunks.budget.ui.transactions_dc])
   (:require-macros
     [devcards.core :refer [defcard]]))
