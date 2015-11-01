a1d4c57523ea76628f54a938c76eaf7edbc4d04f
Petter Eriksson <ipetterik@gmail.com>
2015-11-01 18:03:13 +0100

    Removes DatePicker

--- src/client/flipmunks/budget/ui/add_transaction.cljs
+++ src/client/flipmunks/budget/ui/add_transaction.cljs
@@ -2,47 +2,16 @@
   (:require [om.next :as om :refer-macros [defui]]
             [flipmunks.budget.ui :refer [style]]
             [sablono.core :as html :refer-macros [html]]
-            [garden.core :refer [css]]
-            [goog.events])
-  (:import [goog.ui InputDatePicker]
-           [goog.i18n DateTimeParse DateTimeFormat]
-           [goog events]))
+            [garden.core :refer [css]]))
 
 (defn input [this k opts]
   [:input (-> opts
               (assoc :on-change #(om/update-state! this k (.-value (.-target %)))))])
 
-(defonce datepicker-atom (atom nil))
-
 (defui AddTransaction
        om/IQuery
        (query [this] [:query/all-currencies])
        Object
-       (componentWillMount
-         [this]
-         (let [pattern "MM'/'dd'/'yyyy"
-               formatter (DateTimeFormat. pattern)
-               parser (DateTimeParse. pattern)
-               dp (InputDatePicker. formatter parser)
-               ;;{:keys [ui.add-transaction/datepicker]} (om/get-state this)
-               ]
-
-           (events/listen dp goog.ui.DatePicker.Events.CHANGE
-                          #(om/update-state! this
-                                             :ui.add-transaction/edit-date
-                                             (.-date %)))
-
-           (reset! datepicker-atom dp)))
-       (componentDidMount
-         [this]
-         (let [d (.getElementById js/document "datetimepicker")]
-           (prn d)
-           (prn @datepicker-atom)
-           (.decorate @datepicker-atom d)))
-       (componentWillUnmount
-         [this]
-         (.dispose @datepicker-atom)
-         (reset! datepicker-atom nil))
        (render
          [this]
          (let [{:keys [query/all-currencies]} (om/props this)
@@ -72,7 +41,7 @@
                               %)
                      all-currencies)]]
               [:div [:span "Date:"]
-               [:input#datetimepicker {:type "text"}]]
+               [:input {:type "text" :value "ENTER DATE LOL"}]]
               [:div [:span "Title:"]
                (input this :ui.add-transaction/edit-title {:type        "text"
                                                            :placeholder "enter title"
