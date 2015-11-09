81d3ca42b95448d7c2a726d62a7ef2b896492ef3
Petter Eriksson <ipetterik@gmail.com>
2015-11-09 17:11:16 +0100

    An attempt of my own modal

--- src/client/flipmunks/budget/ui/modal.cljs
+++ src/client/flipmunks/budget/ui/modal.cljs
@@ -3,3 +3,33 @@
             [flipmunks.budget.ui :refer [style]]
             [sablono.core :as html :refer-macros [html]]
             [garden.core :refer [css]]))
+
+(defui Modal
+  Object
+  (render
+    [this]
+    (let [{:keys [dialog-fn]} (om/props this)]
+      (html
+        [:div
+         [:style (css [:#modaldialog
+                       {:top              0
+                        :bottom           0
+                        :right            0
+                        :left             0
+                        :position         "fixed"
+                        :z-index          99999
+                        :opacity          1
+                        :background "rgba(0,0,0,0.6)"
+                        :background-color "#123"}
+                       [:div {:width         "40em"
+                              :position      :relative
+                              :opacity       1
+                              :margin        "10% auto"
+                              :padding       "5px 20px 13px 20px"
+                              :border-radius "10px"
+                              :background-color    "#eee"
+                              :color "000"}]])]
+         [:div#modaldialog
+          (dialog-fn)]]))))
+
+(def ->Modal (om/factory Modal))
--- test/devcards/flipmunks/budget/ui/modal_dc.cljs
+++ test/devcards/flipmunks/budget/ui/modal_dc.cljs
@@ -1,8 +1,14 @@
 (ns flipmunks.budget.ui.modal_dc
   (:require [devcards.core :as dc]
-            [flipmunks.budget.ui.modal :as n]
+            [flipmunks.budget.ui.modal :as m]
             [sablono.core :refer-macros [html]])
   (:require-macros [devcards.core :refer [defcard]]))
 
 (defcard generated-card
-  (html [:div "generated. remove me from flipmunks.budget.ui.modal"]))
+  (m/->Modal {:dialog-fn #(html [:div "ModalDialog :) with a lot of wordsdskoadks akdsdsakdoskdks dsoakdos dksaokdsoak dosak dosak odksaok dsaok dosak dosak odsak odksao dksaopk dsapk dosapk dospak dopsak dopsak dospak das"
+                                 [:p "foo"]
+                                 [:p "foo"]
+                                 [:p "foo"]
+                                 [:p "foo"]
+                                 [:p "foo"]
+                                 ])}))
