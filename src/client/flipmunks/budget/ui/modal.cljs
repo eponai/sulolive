0cf55492390b552874d7bf55bc0b3245cd639bfa
Petter Eriksson <ipetterik@gmail.com>
2015-11-12 12:36:14 +0100

    We have our own modal component

--- src/client/flipmunks/budget/ui/modal.cljs
+++ src/client/flipmunks/budget/ui/modal.cljs
@@ -8,10 +8,10 @@
   Object
   (render
     [this]
-    (let [{:keys [dialog-fn]} (om/props this)]
+    (let [{:keys [dialog-content on-close title]} (om/props this)]
       (html
         [:div
-         [:style (css [:#modaldialog
+         [:style (css [:#modal
                        {:top              0
                         :bottom           0
                         :right            0
@@ -19,17 +19,28 @@
                         :position         "fixed"
                         :z-index          99999
                         :opacity          1
-                        :background "rgba(0,0,0,0.6)"
+                        :background       "rgba(0,0,0,0.6)"
                         :background-color "#123"}
-                       [:div {:width         "40em"
-                              :position      :relative
-                              :opacity       1
-                              :margin        "10% auto"
-                              :padding       "5px 20px 13px 20px"
-                              :border-radius "10px"
-                              :background-color    "#eee"
-                              :color "000"}]])]
-         [:div#modaldialog
-          (dialog-fn)]]))))
+                       [:div#modal-dialog
+                        {:position         :relative
+                         :opacity          1
+                         :margin           "10% auto"
+                         :max-width        "400px"
+                         :padding          "5px 20px 13px 20px"
+                         :border-radius    "10px"
+                         :background-color "#eee"
+                         :color            "000"}]])]
+         [:div#modal
+          [:div#click-outside-target (merge (style {:top      0
+                                                    :bottom   0
+                                                    :right    0
+                                                    :left     0
+                                                    :position "fixed"})
+                                            {:on-click on-close})]
+          [:div#modal-dialog
+           [:div#modal-header
+            [:button {:type "button" :class "close" :on-click on-close} "x"]]
+            [:h4 {:class "modal-title"} title]
+           [:div#modal-content (dialog-content)]]]]))))
 
 (def ->Modal (om/factory Modal))
--- test/devcards/flipmunks/budget/ui/modal_dc.cljs
+++ test/devcards/flipmunks/budget/ui/modal_dc.cljs
@@ -1,11 +1,28 @@
 (ns flipmunks.budget.ui.modal_dc
   (:require [devcards.core :as dc]
+            [om.next :as om :refer-macros [defui]]
             [flipmunks.budget.ui.modal :as m]
             [sablono.core :refer-macros [html]])
   (:require-macros [devcards.core :refer [defcard]]))
 
+(defui ModalCard
+  Object
+  (render [this]
+          (let [{:keys [::show-modal]} (om/get-state this)]
+            (html
+             [:div
+              [:button {:on-click #(om/update-state! this assoc ::show-modal true)}
+               "show modal"]
+              (when show-modal
+                (m/->Modal (assoc (om/props this)
+                             :on-close
+                             #(om/update-state! this assoc ::show-modal false))))]))))
+
+(def ->ModalCard (om/factory ModalCard))
+
 (defcard generated-card
-  (m/->Modal {:dialog-fn #(html [:div "ModalDialog :) with a lot of wordsdskoadks akdsdsakdoskdks dsoakdos dksaokdsoak dosak dosak odksaok dsaok dosak dosak odsak odksao dksaopk dsapk dosapk dospak dopsak dopsak dospak das"
+  (->ModalCard {:title "My Modal"
+                :dialog-content #(html [:div "ModalDialog :) with a lot of wordsdskoadks akdsdsakdoskdks dsoakdos dksaokdsoak dosak dosak odksaok dsaok dosak dosak odsak odksao dksaopk dsapk dosapk dospak dopsak dopsak dospak das"
                                  [:p "foo"]
                                  [:p "foo"]
                                  [:p "foo"]
