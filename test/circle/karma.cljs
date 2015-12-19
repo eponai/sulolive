beb56671f3830ea509c66cf1393a50faae7f2dd5
Petter Eriksson <ipetterik@gmail.com>
2015-12-19 17:31:04 +0700

    Added a karma test which succeeds

--- test/circle/karma.cljs
+++ test/circle/karma.cljs
@@ -3,7 +3,14 @@
 (enable-console-print!)
 
 (defn ^:export run-tests-for-karma []
-  (.info js/__karma__ (clj->js {:total 0}))
+  (.info js/__karma__ (clj->js {:total 1}))
+  (.result js/__karma__ (clj->js {:id "no-op"
+                                  :description "test test"
+                                  :suite "circle.karma"
+                                  :success true
+                                  :skipped nil
+                                  :time 1
+                                  :log ["testing" "the" "thing"]}))
   (prn "running tests for karma")
   (.complete js/__karma__ (clj->js {})))
 
