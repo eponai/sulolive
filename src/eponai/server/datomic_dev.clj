785a067c9882300496a1b54c6764a6331d1e1851
Petter Eriksson <ipetterik@gmail.com>
2015-12-24 16:12:29 +0700

    Inlines the datomic_mem ns into datomic_dev

--- src/eponai/server/datomic_dev.clj
+++ src/eponai/server/datomic_dev.clj
@@ -1,7 +1,81 @@
 (ns eponai.server.datomic_dev
-  (:require [eponai.server.datomic_mem :as mem]
-            [datomic.api :as d]
-            [environ.core :refer [env]]))
+  (:require [datomic.api :as d]
+            [environ.core :refer [env]]
+            [eponai.server.datomic.transact :as transact]
+            [clojure.tools.reader.edn :as edn]
+            [eponai.server.datomic.pull :as p]
+            [cemerick.friend.credentials :as creds]
+            [clojure.java.io :as io])
+  (:import (java.util UUID)))
+
+(defn schema-file []
+  (io/resource "private/datomic-schema.edn"))
+
+(def currencies {:THB "Thai Baht"
+                 :SEK "Swedish Krona"
+                 :USD "US Dollar"})
+
+(def transactions [{:transaction/uuid       (str (UUID/randomUUID))
+                    :transaction/name       "lunch"
+                    :transaction/date       "2015-10-10"
+                    :transaction/amount     180
+                    :transaction/currency   "THB"
+                    :transaction/created-at 1}
+                   {:transaction/uuid       (str (UUID/randomUUID))
+                    :transaction/name       "coffee"
+                    :transaction/date       "2015-10-10"
+                    :transaction/amount     140
+                    :transaction/currency   "THB"
+                    :transaction/created-at 1}
+                   {:transaction/uuid       (str (UUID/randomUUID))
+                    :transaction/name       "dinner"
+                    :transaction/date       "2015-10-10"
+                    :transaction/amount     350
+                    :transaction/currency   "THB"
+                    :transaction/created-at 1}
+                   {:transaction/uuid       (str (UUID/randomUUID))
+                    :transaction/name       "market"
+                    :transaction/date       "2015-10-11"
+                    :transaction/amount     789
+                    :transaction/currency   "THB"
+                    :transaction/created-at 1}
+                   {:transaction/uuid       (str (UUID/randomUUID))
+                    :transaction/name       "lunch"
+                    :transaction/date       "2015-10-11"
+                    :transaction/amount     125
+                    :transaction/currency   "THB"
+                    :transaction/created-at 1}])
+
+(def test-user "test-user@email.com")
+
+(defn create-new-inmemory-db []
+  (let [uri "datomic:mem://test-db"]
+    (d/delete-database uri)
+    (d/create-database uri)
+    (d/connect uri)))
+
+(defn add-verified-user [conn username]
+  (transact/new-user conn {:username username
+                           :bcrypt   (creds/hash-bcrypt "password")})
+  (println "New user created")
+  (let [user (p/user (d/db conn) username)
+        verification (->> (p/verifications (d/db conn) user :user/email)
+                          first
+                          :db/id)]
+    (transact/add conn verification :verification/status :verification.status/verified)
+    (println "User verified.")))
+
+(defn add-data-to-connection [conn]
+  (let [schema (->> (schema-file) slurp (edn/read-string {:readers *data-readers*}))
+        username test-user]
+    (d/transact conn schema)
+    (println "Schema added.")
+    (add-verified-user conn username)
+    (transact/currencies conn currencies)
+    (println "Currencies added.")
+    (let [{:keys [budget/uuid]} (p/budget (d/db conn) username)]
+      (transact/user-txs conn (mapv #(assoc % :transaction/budget uuid) transactions)))
+    (println "User transactions added.")))
 
 (defonce connection (atom nil))
 
@@ -13,8 +87,8 @@
         (prn (str "Exception:" e " when trying to connect to datomic=" uri))
         (prn "Will try to set up inmemory db...")
         (try
-          (let [mem-conn (mem/create-new-inmemory-db)]
-            (mem/add-data-to-connection mem-conn)
+          (let [mem-conn (create-new-inmemory-db)]
+            (add-data-to-connection mem-conn)
             (prn "Successfully set up inmemory db!")
             mem-conn)
           (catch Exception e
