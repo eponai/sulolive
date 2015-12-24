e1cdd9310bf8ab4068112e02d514569c10e65f7e
Petter Eriksson <ipetterik@gmail.com>
2015-12-24 16:14:37 +0700

    refer's core/main-debug for easier lein repl use

--- env/clj/eponai/repl.clj
+++ env/clj/eponai/repl.clj
@@ -1,7 +1,7 @@
 (ns eponai.repl
   ;; require and use more namespaces to have them
   ;; available when the repl starts.
-  (:require [eponai.server.core :as core]
+  (:require [eponai.server.core :as core :refer [main-debug]]
             [clojure.tools.namespace.repl :refer [refresh]]))
 
 
