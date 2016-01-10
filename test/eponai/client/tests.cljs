ff99f0a7575ef84488522f3283389d6f4da471ac
Petter Eriksson <ipetterik@gmail.com>
2016-01-10 18:05:24 +0700

    When running tests with node, it exists with 1 if tests fail

--- test/eponai/client/tests.cljs
+++ test/eponai/client/tests.cljs
@@ -3,9 +3,24 @@
             [cljsjs.react]
             [eponai.client.ui.add_transaction_test]))
 
+(def test-result (atom {}))
+
+(defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
+  (reset! test-result {:success (cljs.test/successful? m)})
+  (prn "test report:")
+  (prn m))
+
 (defn ^:export run []
   (run-tests
     'eponai.client.ui.add_transaction_test))
 
+(defn ^:export node-run-tests []
+  (run)
+  (let [s (:success @test-result)]
+    (prn "All tests passed=" s)
+    ;; Node specific code to exit the process with exit code 1
+    (when-not s
+      (.exit js/process 1))))
+
 (enable-console-print!)
-(set! *main-cli-fn* run)
+(set! *main-cli-fn* node-run-tests)
