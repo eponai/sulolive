e727a5adf88a4902261aa6807ee1cea7c2488560
Diana Gren <dianagren@gmail.com>
2016-01-15 13:47:41 +0700

    Add ui component for Stripe button and a devcard for testing.

--- test/eponai/devcards/devcards_main.cljs
+++ test/eponai/devcards/devcards_main.cljs
@@ -1,5 +1,6 @@
 (ns eponai.devcards.devcards_main
   (:require
+    [cljsjs.react.dom]
     [devcards.core :as dc]
     [sablono.core :as html :refer-macros [html]]
     [eponai.devcards.ui.header_dc]
@@ -8,7 +9,8 @@
     [eponai.devcards.ui.tag_dc]
     [eponai.devcards.ui.add_transaction_dc]
     [eponai.devcards.ui.transaction_dc]
-    [eponai.devcards.ui.all_transactions_dc])
+    [eponai.devcards.ui.all_transactions_dc]
+    [eponai.devcards.ui.stripe-dc])
   (:require-macros
     [devcards.core :refer [defcard]]))
 
---
+++ test/eponai/devcards/ui/stripe_dc.cljs
@@ -0,0 +1,9 @@
+(ns eponai.devcards.ui.stripe-dc
+  (:require [devcards.core :as dc]
+            [sablono.core :refer-macros [html]]
+            [eponai.client.stripe :as stripe])
+  (:require-macros [devcards.core :refer [defcard]]))
+
+(defcard
+  stripe-checkout
+   (stripe/->Checkout {:data-amount "1000"}))
