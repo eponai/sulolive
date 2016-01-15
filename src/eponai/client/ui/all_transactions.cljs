70963dca11ba34b5b12bc0d53482b2d2c55cb2c6
Petter Eriksson <ipetterik@gmail.com>
2016-01-15 14:26:23 +0700

    Replaces map with for in all_transactions.cljs #99

--- src/eponai/client/ui/all_transactions.cljs
+++ src/eponai/client/ui/all_transactions.cljs
@@ -34,81 +34,74 @@
           by-year-month-day (f/transactions-by-year-month-day transactions)]
       (html
         [:div
-         (map
-           (fn [[year months]]
-             [:div#year-panel
-              (opts {:key   [year]
-                     :style {:display         "flex"
-                             :flex-direction  "row"
-                             :justify-content "flex-start"
-                             :align-items     "flex-start"
-                             :width           "100%"}})
+         (for [[year months] (rseq by-year-month-day)]
+           [:div#year-panel
+            (opts {:key   [year]
+                   :style {:display         "flex"
+                           :flex-direction  "row"
+                           :justify-content "flex-start"
+                           :align-items     "flex-start"
+                           :width           "100%"}})
 
-              [:span#year
-               (opts {:key   [year]
-                      :class "network-name"
-                      :style {:margin-right "0.5em"}})
-               year]
+            [:span#year
+             (opts {:key   [year]
+                    :class "network-name"
+                    :style {:margin-right "0.5em"}})
+             year]
 
-              [:div#months
-               (opts {:key   [year]
-                      :style {:width "100%"}})
-               (map
-                 (fn [[month days]]
-                   [:div#month
-                    (opts {:key   [year month]
-                           :style {:display        "flex"
-                                   :flex-direction "row"
-                                   :align-items    "flex-start"
-                                   :width          "100%"}})
+            [:div#months
+             (opts {:key   [year]
+                    :style {:width "100%"}})
+             (for [[month days] (rseq (into (sorted-map) months))]
+               [:div#month
+                (opts {:key   [year month]
+                       :style {:display        "flex"
+                               :flex-direction "row"
+                               :align-items    "flex-start"
+                               :width          "100%"}})
 
-                    [:p#month-name
-                     (opts {:key   [year month]
-                            :class "network-name"})
-                     (f/month-name month)]
+                [:p#month-name
+                 (opts {:key   [year month]
+                        :class "network-name"})
+                 (f/month-name month)]
 
-                    [:div#days
-                     (opts {:key   [year month]
-                            :class "panel-group"
-                            :style {:width "100%"}})
-                     (map
-                       (fn [[day {:keys [date transactions]}]]
-                         [:div#day
-                          (opts {:key   [year month day]
-                                 :class "panel panel-info"})
+                [:div#days
+                 (opts {:key   [year month]
+                        :class "panel-group"
+                        :style {:width "100%"}})
+                 (for [[day {:keys [date transactions]}] (rseq (into (sorted-map) days))]
+                   [:div#day
+                    (opts {:key   [year month day]
+                           :class "panel panel-info"})
 
-                          [:div#weekday
-                           (opts {:key      [year month day]
-                                  :class    "panel-heading list-group-item"
-                                  :style    {:display         "flex"
-                                             :flex-direction  "row"
-                                             :justify-content "space-between"}
-                                  :on-click #(mutate/cas!
-                                              this
-                                              (:db/id date)
-                                              ::day-expanded?
-                                              (::day-expanded? date)
-                                              (not (::day-expanded? date)))})
-                           [:span#dayname
-                            (opts {:key   [year month day]
-                                   :style {:margin-right "0.3em"}})
-                            (str (f/day-name date) " " day)]
+                    [:div#weekday
+                     (opts {:key      [year month day]
+                            :class    "panel-heading list-group-item"
+                            :style    {:display         "flex"
+                                       :flex-direction  "row"
+                                       :justify-content "space-between"}
+                            :on-click #(mutate/cas!
+                                        this
+                                        (:db/id date)
+                                        ::day-expanded?
+                                        (::day-expanded? date)
+                                        (not (::day-expanded? date)))})
+                     [:span#dayname
+                      (opts {:key   [year month day]
+                             :style {:margin-right "0.3em"}})
+                      (str (f/day-name date) " " day)]
 
-                           [:span#daysum
-                            (opts {:key [year month day]})
-                            (let [{:keys [currency amount]} (sum transactions)]
-                              (str amount " " (or (:currency/symbol-native currency)
-                                                  (:currency/code currency))))]]
+                     [:span#daysum
+                      (opts {:key [year month day]})
+                      (let [{:keys [currency amount]} (sum transactions)]
+                        (str amount " " (or (:currency/symbol-native currency)
+                                            (:currency/code currency))))]]
 
-                          (when (::day-expanded? date)
-                            [:div#transactions
-                             (opts {:key   [year month day]
-                                    :class "panel-body"})
+                    (when (::day-expanded? date)
+                      [:div#transactions
+                       (opts {:key   [year month day]
+                              :class "panel-body"})
 
-                             (map trans/->Transaction
-                                  transactions)])])
-                       (rseq (into (sorted-map) days)))]])
-                 (rseq (into (sorted-map) months)))]])
-           (rseq by-year-month-day))]))))
+                       (map trans/->Transaction transactions)])])]])]])]))))
 
 (def ->AllTransactions (om/factory AllTransactions))
