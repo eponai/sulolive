4c55b3538b3bd7e71c760a6a7770f9444dc11952
Petter Eriksson <ipetterik@gmail.com>
2016-01-24 17:39:56 +0700

    Docker img test returns exit codes correctly

--- scripts/run-docker-img-test.sh
+++ scripts/run-docker-img-test.sh
@@ -1,13 +1,18 @@
-#!/bin/bash -eu
+#!/bin/bash -u
 
-PORT=$1
+HOST=$1
+PORT=$2
 
-wget -O- --max-redirect 0 --retry-connrefused -t 10 http://localhost:$PORT
+wget -O- --max-redirect 0 --retry-connrefused -t 1 http://$HOST:$PORT
 exit_code=$?
 
 if [ "$exit_code" = "8" ]; then
   echo "We got redirected and it's fine!"
-else
-  echo "Exit code other than 8, it's not fine. Was: $exit_code"
+  exit 0
+elif [ "$exit_code" = "0" ]; then
+ exit 0 
 fi
+
+echo "Exit code other than 0 or 8, it's not fine. Was: $exit_code"
+exit $exit_code
 
