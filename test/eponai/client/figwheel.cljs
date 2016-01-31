6ce7cc88ef7e5444fc0948112ad44e5e935519df
Petter Eriksson <ipetterik@gmail.com>
2016-01-31 19:00:08 +0700

    Figwheel only runs (app/run) if the url matches an app route

--- test/eponai/client/figwheel.cljs
+++ test/eponai/client/figwheel.cljs
@@ -1,8 +1,17 @@
  (ns eponai.client.figwheel
-  (:require [eponai.client.app :as app]
-            [eponai.client.tests :as tests]))
+  (:require [bidi.bidi :as bidi]
+            [eponai.client.routes :as routes]
+            [eponai.client.app :as app]
+            [eponai.client.tests :as tests]
+            [taoensso.timbre :refer-macros [warn debug]]))
 
 
 (defn ^:export run []
- (tests/run)
- (app/run))
+  (tests/run)
+  (let [url js/location.pathname]
+    (debug "Url is:" url)
+    (if (bidi/match-route routes/routes js/location.pathname)
+     (do
+       (debug "Matched an app route. Calling (app/run)")
+       (app/run))
+     (warn "Url did not match an app route. Figwheel will not call (app/run)"))))
