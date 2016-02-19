0bdcd0064f1251abd2559535c0423ae3134a116d
Petter Eriksson <ipetterik@gmail.com>
2016-02-19 18:58:31 +0800

    Describes the filter namespace

--- src/eponai/server/datomic/filter.clj
+++ src/eponai/server/datomic/filter.clj
@@ -1,34 +1,26 @@
 (ns eponai.server.datomic.filter
+  "Filter maps can be combined, incrementally updated and
+  applied as either AND or OR filters.
+
+  A filter map is defined as:
+  {:f filter-function
+   :props map-of-values-which-describe-a-filter}.
+
+  The filter function takes keys from :props as input
+  and returns a datomic filter function (fn [db datom]).
+
+  Each key in props contain:
+  {:init initial-value
+  :update-fn (fn [db old-val] return new-val)}
+  The purpose of the :props is to describe a filter to be incrementaly
+  updated.
+
+  It sounds more complicated than it is..?
+  Hopefully overtime, we figure out an easier way to describe
+  incrementally updatable database filters."
   (:require [clojure.set :as s]
             [datomic.api :as d]
             [taoensso.timbre :refer [debug]]))
-
-;; TODO: Using :user/email right now. Should use :user/uuid when it's done.
-(def user-owned-rule
-  '[[(owner? ?user-id ?e)
-     [?e :user/email ?user-id]]
-    [(owner? ?user-id ?e)
-     [?e ?ref-attr ?ref]
-     (owner? ?user-id ?ref)]])
-
-(defn user-entities [db user-id]
-  (d/q '{:find  [[?e ...]]
-         :in    [$ ?user-id %]
-         :where [(owner? ?user-id ?e)]}
-       db
-       user-id
-       user-owned-rule))
-
-(defn user-attributes [db]
-  ;;TODO: Add more user entities? We want to filter out anything that has to do with sensitive
-  ;;      user data. Such as what they have bought.
-  (let [user-entity-keys [:budget/uuid :transaction/uuid :dashboard/uuid]]
-    (d/q '{:find  [[?a ...]]
-           :in    [$ [?user-entity-key ...]]
-           :where [[?e ?user-entity-key]
-                   [?e ?a]]}
-         db
-         user-entity-keys)))
 
 ;; Updating and applying filters
 
@@ -68,7 +60,7 @@
              props))
 
 (defn apply-filters
-  "Applies filters to a db."
+  "Applies filters with AND to a db."
   [db filters]
   {:pre [(sequential? filters)]}
   (reduce (fn [db {:keys [f props]}]
@@ -93,52 +85,57 @@
             (first filter-maps)
             (rest filter-maps))))
 
-;; -------------- Filter maps --------------
-;; Filter maps can be combined, incrementally updated, applied used as
-;; AND or OR filters.
-;; A filter map is defined as:
-;; {:f filter-function
-;;  :props map-of-values-which-describe-a-filter}.
-;; Each key in props contain:
-;; {:init initial-value
-;;  :update-fn (fn [db old-val] return new-val)}
-;; The filter map's :f function, will be passed a map of props with
-;; their values bound to the return value of the last time update-fn
-;; was called.
-;; It sounds more complicated than it is..?
-;; Hopefully overtime, we figure out an easier way to describe
-;; incrementally updatable database filters.
+;; TODO: Using :user/email right now. Should use :user/uuid when it's done.
+(def user-owned-rule
+  '[[(owner? ?user-id ?e)
+     [?e :user/email ?user-id]]
+    [(owner? ?user-id ?e)
+     [?e ?ref-attr ?ref]
+     (owner? ?user-id ?ref)]])
 
-(def user-attrs-filter {:init      #{}
-                        :update-fn (fn [db old-val] (into old-val (user-attributes db)))})
+(defn user-entities [db user-id]
+  (d/q '{:find  [[?e ...]]
+         :in    [$ ?user-id %]
+         :where [(owner? ?user-id ?e)]}
+       db
+       user-id
+       user-owned-rule))
 
-(defn- user-or-public-entity-filter-map
-  "Keeps a specific user's entities and public entities.
-  Can be incrementally updated with the :update-props function."
-  [user-id]
+(defn user-attributes [db]
+  ;;TODO: Add more user entities? We want to filter out anything that has to do with sensitive
+  ;;      user data. Such as what they have bought.
+  (let [user-entity-keys [:budget/uuid :transaction/uuid :dashboard/uuid]]
+    (d/q '{:find  [[?a ...]]
+           :in    [$ [?user-entity-key ...]]
+           :where [[?e ?user-entity-key]
+                   [?e ?a]]}
+         db
+         user-entity-keys)))
+
+(defn- user-entities-filter-map [user-id]
   {:props {:owned-entities {:init      #{}
-                            :update-fn (fn [db old-val] (into old-val (user-entities db user-id)))}}
+                            :update-fn (fn [db old-val]
+                                         (into old-val (user-entities db user-id)))}}
    :f     (fn [{:keys [owned-entities]}]
             {:pre [(set? owned-entities)]}
             (fn [_ [eid]]
               (contains? owned-entities eid)))})
 
-(defn- no-user-data []
-  {:props {:user-attrs user-attrs-filter}
+(defn- public-entities-filter-map []
+  {:props {:user-attrs {:init      #{}
+                        :update-fn (fn [db old-val]
+                                     (into old-val (user-attributes db)))}}
    :f     (fn [{:keys [user-attrs]}]
             {:pre [(set? user-attrs)]}
             (fn [_ [_ attr]]
               (not (contains? user-attrs attr))))})
 
-(defn- no-auth-filter-map []
-  (no-user-data))
-
 (defn authenticated-db-filters [user-id]
-  [(or-filter (user-or-public-entity-filter-map user-id)
-              (no-user-data))])
+  [(or-filter (user-entities-filter-map user-id)
+              (public-entities-filter-map))])
 
 (defn not-authenticated-db-filters []
-  [(no-auth-filter-map)])
+  [(public-entities-filter-map)])
 
 ;; Deprecated, using for testing.
 (defn filter-db [db filters]
