a1254c3f833a36d75996c589b46b8622b1edd157
Petter Eriksson <ipetterik@gmail.com>
2016-05-31 11:21:28 -0400

    Extends IEquiv on goog.dates

--- src/eponai/client/utils.cljs
+++ src/eponai/client/utils.cljs
@@ -1,7 +1,31 @@
 (ns eponai.client.utils
   (:require [devtools.core :as devtools]
             [eponai.client.logger :as logger]
+            [goog.date]
+            [cljs-time.coerce :as cljs-time]
             [taoensso.timbre :as timbre :refer-macros [debug]]))
+
+;; ------ Dates -----------
+;; Extends equality on goog.date's to speed up shouldComponentUpdate.
+
+(defn date-eq [a b]
+  (and (satisfies? cljs-time/ICoerce a)
+       (satisfies? cljs-time/ICoerce b)
+       (= (cljs-time/to-long a)
+          (cljs-time/to-long b))))
+
+
+(extend-protocol IEquiv
+  goog.date.Date
+  (-equiv [this other] (date-eq this other))
+
+  goog.date.DateTime
+  (-equiv [this other] (date-eq this other))
+
+  goog.date.UtcDateTime
+  (-equiv [this other] (date-eq this other)))
+
+;; ------ Util functions -----------
 
 (defn distinct-by
   "Distinct by (f input). See clojure.core/distinct."
@@ -19,6 +43,8 @@
                   (rf result input)))))))))
   ([f coll]
    (sequence (distinct-by f) coll)))
+
+;; ------ App initialization -----------
 
 (defn set-level [l]
   (timbre/set-level! l))
