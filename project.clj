a351592b57df7cf600caa1a2e75525f05d4f2bd7
Petter Eriksson <ipetterik@gmail.com>
2016-07-27 13:03:23 +0200

    lein all-deps runs pod install

--- project.clj
+++ project.clj
@@ -99,7 +99,9 @@
                                    ]
 
   :aliases {"all-deps"               ^{:doc "Fetches both clj, cljs and node dependencies."}
-                                     ["do" "deps"
+                                     ["do" 
+                                      "deps"
+                                      ["shell" "pod" "install" "--project-directory=./ios"]
                                       ["shell" "npm" "install"]]
             "prod-build-ios-local"   ^{:doc "Recompile mobile code with production profile.
                                            The build runs against a local/laptop server."}
