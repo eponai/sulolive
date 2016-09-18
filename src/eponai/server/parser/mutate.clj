c7b93a7fcfda798ae8f04fdf22558f6517b08fd8
Petter Eriksson <ipetterik@gmail.com>
2016-09-18 21:06:57 +0200

    Creates messages for server side mutations

--- src/eponai/server/parser/mutate.clj
+++ src/eponai/server/parser/mutate.clj
@@ -28,9 +28,9 @@
 ;; ------------------- Transaction --------------------
 
 (defmutation transaction/create
-  [{:keys [state auth] :as env} k input-transaction]
-  {:success (str "Transaction: \"" (:transaction/title input-transaction) "\" created!")
-   :error   "Error creating transaction"}
+  [{:keys [state auth] :as env} k {:keys [transaction/title] :as input-transaction}]
+  {:success (str "Created transaction " title)
+   :error   (str "Error creating transaction " title)}
   {:action (fn []
              (debug "transaction/create with params:" input-transaction)
              (validate/validate env k {:transaction input-transaction
@@ -41,9 +41,10 @@
                (async/go (async/>! currency-chan (:transaction/date transaction)))
                (assoc tx-report :currency-chan currency-chan)))})
 
-(defmethod mutate 'transaction/edit
-  [{:keys [state auth] :as env} k {:keys [transaction/uuid] :as transaction}]
-  (debug "transaction/edit with params:" transaction)
+(defmutation transaction/edit
+  [{:keys [state auth] :as env} k {:keys [transaction/uuid transaction/title] :as transaction}]
+  {:success (str "Edited transaction " title)
+   :error   (str "Error editing transaction " title)}
   {:action (fn []
              (validate/validate env k {:transaction transaction
                                        :user-uuid (:username auth)})
@@ -54,69 +55,76 @@
 
 ;; ----------------- project --------------------
 
-(defmethod mutate 'project/save
+(defmutation project/save
   [{:keys [state auth]} _ params]
-  (debug "project/save with params: " params)
+  ["Project saved" "Unable to save project"]
   {:action (fn []
+             (debug "project/save with params: " params)
              (let [user-ref [:user/uuid (:username auth)]
                    project (format/project user-ref params)
                    dashboard (format/dashboard (:db/id project) params)]
                (transact/transact state [project dashboard])))})
 
-(defmethod mutate 'project/share
+(defmutation project/share
   [{:keys [state]} _ {:keys [project/uuid user/email] :as params}]
-  (debug "project/save with params: " params)
+  ["Shared project" "Unable to share project"]
   {:action (fn []
+             (debug "project/save with params: " params)
              (api/share-project state uuid email))})
 
 ;; --------------- Widget ----------------
 
-(defmethod mutate 'widget/create
+(defmutation widget/create
   [{:keys [state auth] :as env} k params]
-  (debug "widget/create with params: " params)
+  ["Created widget" "Error creating widget"]
   {:action (fn []
+             (debug "widget/create with params: " params)
              (validate/validate env k {:widget    params
                                        :user-uuid (:username auth)})
              (let [widget (format/widget-create params)]
                (transact/transact-one state widget)))})
 
-(defmethod mutate 'widget/edit
+(defmutation widget/edit
   [{:keys [state]} _ params]
-  (debug "widget/edit with params: " params)
+  ["Edited widget" "Error editing widget"]
   {:action (fn []
+             (debug "widget/edit with params: " params)
              (let [widget (format/widget-edit params)]
                (transact/transact state widget)))
    :remote true})
 
-(defmethod mutate 'widget/delete
-  [{:keys [state]} _ params]
-  (debug "widget/delete with params: " params)
-  (let [widget-uuid (:widget/uuid params)]
-    {:action (fn []
-               (transact/transact-one state [:db.fn/retractEntity [:widget/uuid widget-uuid]]))
-     :remote true}))
+(defmutation widget/delete
+  [{:keys [state]} _ {:keys [widget/uuid] :as params}]
+  ["Deleted widget" "Error deleting widget"]
+  {:action (fn []
+             (debug "widget/delete with params: " params)
+             (transact/transact-one state [:db.fn/retractEntity [:widget/uuid uuid]]))
+   :remote true})
 
 ;; ---------------- Dashboard ----------------
 
-(defmethod mutate 'dashboard/save
+(defmutation dashboard/save
   [{:keys [state]} _ {:keys [widget-layout] :as params}]
-  (debug "dashboard/save with params: " params)
+  ["Saved dashboard" "Error saving dashboard"]
   {:action (fn []
+             (debug "dashboard/save with params: " params)
              (transact/transact state (format/add-tempid widget-layout)))})
 
 ;; ------------------- User account related ------------------
 
-(defmethod mutate 'settings/save
+(defmutation settings/save
   [{:keys [state]} _ {:keys [currency user] :as params}]
-  (debug "settings/save with params: " params)
+  ["Saved settings" "Error saving settings"]
   {:action (fn []
+             (debug "settings/save with params: " params)
              (transact/transact-one state [:db/add [:user/uuid (:user/uuid user)] :user/currency [:currency/code currency]]))})
 
 
-(defmethod mutate 'stripe/subscribe
+(defmutation stripe/subscribe
   [{:keys [state auth stripe-fn]} _ p]
-  (debug "stripe/subscribe with params:" p)
+  ["Subscribed!" "Error subscribing"]
   (let [db (d/db state)
+        _ (debug "stripe/subscribe with params:" p)
         stripe-eid (p/one-with db {:where '[[?u :user/uuid ?useruuid]
                                             [?e :stripe/user ?u]]
                                    :symbols {'?user-uuid (:username auth)}})
@@ -127,16 +135,18 @@
     {:action (fn []
                (api/stripe-subscribe state stripe-fn stripe-account p))}))
 
-(defmethod mutate 'stripe/trial
+(defmutation stripe/trial
   [{:keys [state auth stripe-fn]} _ _]
+  ["Started trial" "Error starting trial"]
   (let [user (p/pull (d/db state) [:user/email :stripe/_user] [:user/uuid (:username auth)])]
     {:action (fn []
                (api/stripe-trial state stripe-fn user))}))
 
-(defmethod mutate 'stripe/cancel
+(defmutation stripe/cancel
   [{:keys [state auth] :as env} _ p]
-  (debug "stripe/cancel with params:" p)
+  ["Subscription cancelled" "Error cancelling subscription"]
   (let [db (d/db state)
+        _ (debug "stripe/cancel with params:" p)
         eid (p/one-with db {:where   '[[?u :user/uuid ?user-uuid]
                                        [?e :stripe/user ?u]]
                             :symbols {'?user-uuid (:username auth)}})
@@ -148,10 +158,11 @@
 
 ;; ############# Session mutations #################
 
-(defmethod mutate 'session.signin/email
+(defmutation session.signin/email
   [{:keys [state]} k {:keys [device] :as params}]
-  (debug "signup/email with params:" params)
+  ["Signed in via email" "Error signing in via email"]
   {:action (fn []
+             (debug "signup/email with params:" params)
              ;; TODO: Need a more generic way of specifying required parameters for mutations.
              (when-not device
                (throw (ex-info (str "No device specified for " k
@@ -161,26 +172,30 @@
              (-> (api/signin state (:input-email params))
                  (assoc :device device)))})
 
-(defmethod mutate 'session.signin.email/verify
+(defmutation session.signin.email/verify
   [{:keys [auth]} _ {:keys [verify-uuid] :as p}]
-  (debug "session.signin.email/verify with params: " p)
+  ["Verified signin via email" "Error verifying signin via email"]
   {:action (fn []
+             (debug "session.signin.email/verify with params: " p)
              {:auth (some? auth)})})
 
-(defmethod mutate 'session.signin/facebook
+(defmutation session.signin/facebook
   [{:keys [auth]} _ {:keys [access-token user-id] :as p}]
-  (debug "session.signin/facebook with params: " p)
+  ["Signed in via facebook" "Error signing in via facebook"]
   {:action (fn []
+             (debug "session.signin/facebook with params: " p)
              {:auth (some? auth)})})
 
-(defmethod mutate 'session.signin/activate
+(defmutation session.signin/activate
   [{:keys [auth]} _ {:keys [user-uuid user-email] :as p}]
-  (debug "session.signin/activate with params: " p)
+  ["Activate successful" "Error activating"]
   {:action (fn []
+             (debug "session.signin/activate with params: " p)
              {:auth (some? auth)})})
 
-(defmethod mutate 'session/signout
+(defmutation session/signout
   [{:keys [auth]} _ p]
-  (debug "session/signout with params: " p)
+  ["Signout complete" "Error signing out"]
   {:action (fn []
+             (debug "session/signout with params: " p)
              {:auth (some? auth)})})
