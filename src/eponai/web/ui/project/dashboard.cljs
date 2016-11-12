a7a9e99bd784a991544d6394c1baef3f6a82c047
Petter Eriksson <ipetterik@gmail.com>
2016-11-12 15:18:52 +0100

    Dashboard has a date-range picker to choose time period

--- src/eponai/web/ui/project/dashboard.cljs
+++ src/eponai/web/ui/project/dashboard.cljs
@@ -5,6 +5,7 @@
     [eponai.web.ui.project.all-transactions :refer [Transaction]]
     [eponai.web.ui.d3.balance-chart :as bc]
     [eponai.web.ui.d3.pie-chart :as pc]
+    [eponai.web.ui.daterangepicker :refer [->DateRangePicker]]
     [eponai.web.ui.icon :as icon]
     [om.next :as om :refer-macros [defui]]
     [goog.string :as gstring]
@@ -22,21 +23,27 @@
     ['({:query/transactions ?transaction} {:filter ?filter})
      {:proxy/quick-add-transaction (om/get-query at/QuickAddTransaction)}])
   Object
+  (initLocalState [this]
+    {:start-date        (date/first-day-of-this-month)
+     :end-date          (date/last-day-of-this-month)
+     ::on-date-apply-fn (fn [{:keys [start-date end-date selected-range]}]
+                          (om/update-state! this assoc :start-date start-date :end-date end-date))})
   (render [this]
     (let [{:keys [query/transactions proxy/quick-add-transaction]} (om/props this)
+          {:keys [::on-date-apply-fn start-date end-date]} (om/get-state this)
           {:keys [project]} (om/get-computed this)
           {:keys [housing limit transport spent avg-daily-spent left-by-end budget]} (report/summary transactions)
-          balance-report (report/balance-vs-spent transactions)
-          ]
+          balance-report (report/balance-vs-spent transactions)]
       (html
         [:div#dashboard
          (at/->QuickAddTransaction (om/computed quick-add-transaction
                                                 {:project project}))
          [:div.row.align-center
-          [:a.button.black.hollow.month
-           (date/date->string (date/today) "MMMM")
-           ;[:i.fa.fa-caret-down.fa-fw]
-           ]]
+          (->DateRangePicker (om/computed {:single-calendar? false
+                                           :start-date       start-date
+                                           :end-date         end-date}
+                                          {:on-apply on-date-apply-fn
+                                           :format   "MMM dd"}))]
          [:div.content-section
           [:div.row.section-title
            (icon/dashboard-balance)
