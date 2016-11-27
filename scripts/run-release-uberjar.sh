6e516304f7c09c54331d3f4a3d48e91a9fc97cfc
Petter Eriksson <ipetterik@gmail.com>
2016-11-27 13:54:19 +0100

    Adds script to run jourmoney with uberjar

---
+++ scripts/run-release-uberjar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script_dir=$(dirname $0)
+
+export TIMBRE_LEVEL=":info"
+export SESSION_COOKIE_STORE_KEY="a 16 bits secret"
+export SESSION_COOKIE_name="cookie-name"
+export CLJS_BUILD_ID=release 
+export JOURMONEY_SERVER_URL_SCHEMA="http"
+export JOURMONEY_SERVER_URL_SCHEMA="http"
+export JOURMONEY_SERVER_URL_HOST="localhost:3000"
+
+cd "$script_dir"
+lein uberjar
+java -server -Xmx4g -Dclojure.compiler.direct-linking=true -cp ./target/uberjar/budget-0.1.0-SNAPSHOT-standalone.jar eponai.server.core
