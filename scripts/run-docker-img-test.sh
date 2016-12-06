a3fb191e35ef383d6ca5514f4f07e8dedd6761c8
Petter Eriksson <ipetterik@gmail.com>
2016-12-05 20:20:16 -0500

    Adds user/pass to run-docker-img-test.sh, because we have user/pass on our prototype

--- scripts/run-docker-img-test.sh
+++ scripts/run-docker-img-test.sh
@@ -4,7 +4,7 @@ HOST=$1
 PORT=$2
 
 echo "Test with forwarding (which in production is done by load balancer)"
-wget -O- --header "x-forwarded-proto: https" --max-redirect 0 --retry-connrefused -t 30 http://$HOST:$PORT
+wget -O- --user=sulo --password=hejsan --header "x-forwarded-proto: https" --max-redirect 0 --retry-connrefused -t 30 http://$HOST:$PORT
 
 exit_code=$?
 if [ "$exit_code" = "0" ]; then
@@ -15,7 +15,7 @@ else
 fi
 
 echo "Test with redirect"
-wget -O- --max-redirect 0 --retry-connrefused -t 10 http://$HOST:$PORT
+wget -O- --user=sulo --password=hejsan --max-redirect 0 --retry-connrefused -t 10 http://$HOST:$PORT
 exit_code=$?
 
 if [ "$exit_code" = "8" ]; then
