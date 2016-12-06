86e30911570ef03c2d672073e4238ca1dbcbe075
Petter Eriksson <ipetterik@gmail.com>
2016-12-05 23:08:27 -0500

    Adds explicit region for deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,7 @@ docker push $DOCKER_IMAGE
 EB_BUCKET=sulo-conf
 EB_APP_NAME=sulo-live
 EB_ENV_NAME=Custom-env-1
+REGION=us-west-2
 
 # Create new Elastic Beanstalk version
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
@@ -20,11 +21,11 @@ DOCKERRUN_S3_FILE=docker/$EB_APP_NAME/$EB_ENV_NAME/$DOCKERRUN_FILE
 
 # Using comma (,) instead of slash (/) in sed because DOCKER_IMAGE contains slashes
 sed "s,<DOCKER_IMAGE>,$DOCKER_IMAGE," < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_S3_FILE
-aws elasticbeanstalk create-application-version --application-name $EB_APP_NAME \
+aws --region=$REGION s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_S3_FILE
+aws --region=$REGION elasticbeanstalk create-application-version --application-name $EB_APP_NAME \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_S3_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name $EB_ENV_NAME \
+aws --region=$REGION elasticbeanstalk update-environment --environment-name $EB_ENV_NAME \
     --version-label $SHA1
 
