79432687619bf400a3b44adb9a8f1e827d473972
Diana Gren <dianagren@gmail.com>
2016-12-07 15:01:29 -0500

    Fix stream settings.

--- src/eponai/web/ui/stream.cljs
+++ src/eponai/web/ui/stream.cljs
@@ -17,29 +17,31 @@
         (debug "subscriber: " subscriber " view " view)
         (.on subscriber "*" js/window.red5proHandleSubscriberEvent)
         (.. subscriber
-            (setPlaybackOrder #js ["hls"])
+            (setPlaybackOrder #js ["rtmp" "hls"])
             (init
               #js {
                    :rtmp #js {:protocol          "rtmp"     ;(if is-secure "wss" "ws")
-                              :host              "192.168.0.32"
+                              :host              "localhost"
                               :port              1935       ;(if is-secure "8083" "8081")
                               :width             "1000px"
                               :height            "750px"
                               :mimeType          "rtmp/flv"
-                              :useVideoJS        true
+                              :useVideoJS        false
                               :swf               "/lib/red5pro/red5pro-subscriber.swf"
                               :swfobjectURL      "/lib/swfobject/swfobject.js"
                               :productInstallURL "/lib/swfobject/playerProductInstall.swf"
                               :app               "live"
-                              :streamName        "mystream"}
+                              :streamName        "first"}
                    :hls  #js {:protocol          "http"
+                              :host              "localhost"
                               :port              5080
-                              :streamName        "mystream"
+                              :streamName        "first"
                               :mimeType          "application/x-mpegURL"
                               :swf               "/lib/red5pro/red5pro-video-js.swf"
                               :swfobjectURL      "/lib/swfobject/swfobject.js"
                               :productInstallURL "/lib/swfobject/playerProductInstall.swf"}})
             (then (fn []
+                    (debug "Subscribed")
                     (.off subscriber "*" js/window.red5proHandleSubscriberEvent)
                     ))
             (catch (fn [error]
