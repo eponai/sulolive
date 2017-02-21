194c001cf20e9eb45e6e88f338a02ace41d1ac15
Petter Eriksson <ipetterik@gmail.com>
2017-02-20 17:18:34 -0800

    Adds destroy to hls extern

--- src-hacks/js/externs/hls.js
+++ src-hacks/js/externs/hls.js
@@ -3,6 +3,7 @@ Hls.prototype.loadSource;
 Hls.prototype.attachMedia;
 Hls.prototype.detachMedia;
 Hls.prototype.on;
+Hls.prototype.destroy;
 Hls.Events;
 Hls.Events.MANIFEST_PARSED;
 Hls.Events.ERROR;
