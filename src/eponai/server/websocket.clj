7a039f9d7f920289a0ba542535442ab789465c6a
Petter Eriksson <ipetterik@gmail.com>
2017-03-15 16:24:00 -0500

    Adds stop-fn to websocket.clj for stopping reads off of sente's channel

--- src/eponai/server/websocket.clj
+++ src/eponai/server/websocket.clj
@@ -7,28 +7,31 @@
     [taoensso.timbre :refer [debug error]]))
 
 (defn chat-websocket []
-  (let [sente-channel (sente/make-channel-socket! (sente.aleph/get-sch-adapter)
-                                                  {:packer     (sente-transit/get-transit-packer)
-                                                   :user-id-fn (fn [ring-req]
-                                                                 (or (get-in ring-req [:identity :email])
-                                                                     ;; client-id is assoc'ed by sente
-                                                                     ;; before calling this fn.
-                                                                     (:client-id ring-req)))})
+  (let [{:keys [ch-recv] :as sente-channel}
+        (sente/make-channel-socket! (sente.aleph/get-sch-adapter)
+                                    {:packer     (sente-transit/get-transit-packer)
+                                     :user-id-fn (fn [ring-req]
+                                                   (or (get-in ring-req [:identity :email])
+                                                       ;; client-id is assoc'ed by sente
+                                                       ;; before calling this fn.
+                                                       (:client-id ring-req)))})
         control-chan (a/chan 1)
         event-handler (fn [{:keys [event id ?data send-fn ?reply-fn uid ring-req client-id]}]
-                        (debug "Client-id: " [:uid uid :client client-id] " sent chat event: " event))]
+                        (debug "Client-id: " [:uid uid :client client-id]
+                               " sent chat event: " event))]
     (go
       (loop []
-        (if-let [control-value (a/poll! control-chan)]
-          (debug "Exiting chat websocket due to value on control-channel: " control-value)
-          (do
-            (try
-              (event-handler (<! (:ch-recv sente-channel)))
-              (catch Exception e
-                (error "Exception in chat-websocket: " e)
-                (debug "Will continue chat-websocket until there's an Error."))
-              (catch Error e
-                (error "Error in chat-websocket: " e)
-                (a/put! control-chan e)))
-            (recur)))))
-    sente-channel))
+        (let [[v c] (a/alts! [ch-recv control-chan])]
+          (if (= c control-chan)
+            (debug "Exiting chat websocket due to value on control-channel: " v)
+            (do
+              (try
+                (event-handler v)
+                (catch Exception e
+                  (error "Exception in chat-websocket: " e)
+                  (debug "Will continue chat-websocket until there's an Error."))
+                (catch Error e
+                  (error "Error in chat-websocket: " e)
+                  (a/put! control-chan e)))
+              (recur))))))
+    (assoc sente-channel :stop-fn #(a/close! control-chan))))
