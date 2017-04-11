876638147e74b9e362fb35b01f4450f9bc3b8dd3
Petter Eriksson <ipetterik@gmail.com>
2017-04-11 19:51:15 +0200

    When DB_URL starts with datomic:dev://localhost: we setup a valid sulo-db with schema and data

--- src/eponai/server/datomic_dev.clj
+++ src/eponai/server/datomic_dev.clj
@@ -4,6 +4,7 @@
             [clojure.tools.reader.edn :as edn]
             [eponai.server.datomic.mocked-data :as mocked]
             [clojure.java.io :as io]
+            [clojure.string :as str]
             [eponai.common.database :as db]
             [eponai.common.database.functions :as dbfn]
             [taoensso.timbre :refer [debug error info]]))
@@ -48,13 +49,23 @@
 (defn create-connection
   [uri {::keys [add-data?]}]
   (try
-    (if (contains? #{nil "" "test"} uri)
-
+    (cond
+      (contains? #{nil "" "test"} uri)
       (let [_ (info "Setting up inmemory db because uri is set to:" uri)
             mem-conn (create-new-inmemory-db)]
         (add-data-to-connection mem-conn add-data?)
         (debug "Successfully set up inmemory db!")
         mem-conn)
+      (str/starts-with? uri "datomic:dev://localhost:")
+      (if (d/create-database uri)
+        (let [conn (d/connect uri)]
+          (debug "Created a new database for localhost. Adding schema and data")
+          (add-data-to-connection conn add-data?)
+          conn)
+        (do
+          (debug "Connecting to an old localhost database")
+          (d/connect uri)))
+      :else
       (do
         (info "Setting up remote db.")
         (d/connect uri)))
