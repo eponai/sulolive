0cbede0555c02e3c29cf8ac5fc91d2e1e9ba0f6a
Petter Eriksson <ipetterik@gmail.com>
2017-04-22 12:34:58 +0200

    Moves plomber dependency out of main

--- env/client/dev/env/web/main.cljs
+++ env/client/dev/env/web/main.cljs
@@ -1,9 +1,12 @@
 (ns env.web.main
   (:require [eponai.web.app :as app]
-            [eponai.client.devtools :as devtools]))
+            [eponai.client.devtools :as devtools]
+            [plomber.core :as plomber]))
 
 (set! js/window.mixpanel #js {"track" (fn [& args] )})
 
 (defn ^:export runsulo []
   (devtools/install-app)
-  (app/run-dev))
+  (app/run-dev {
+                ;;::plomber   (plomber/instrument)
+                }))
