3fd2ef5c4cfb8f39fc683ddcd1c189ab3541697b
Petter Eriksson <ipetterik@gmail.com>
2017-04-28 15:08:36 +0200

    Makes it easier to test run-release-repl

--- scripts/run-release-repl.sh
+++ scripts/run-release-repl.sh
@@ -3,8 +3,8 @@
 script_dir=$(dirname $0)
 cd "$script_dir/.."
 
-export TIMBRE_LEVEL=":info"
-export PORT="8080"
+# export TIMBRE_LEVEL=":info"
+export PORT="3000"
 export CLJS_BUILD_ID="release"
 
-(echo "(core/-main)"; cat <&0) | lein repl
+(echo "(core/main-release-no-ssl)"; cat <&0) | lein repl
