e6b61ae7a4956abcf72c9786259699c7632a6e61
Petter Eriksson <ipetterik@gmail.com>
2017-06-17 15:48:41 -0700

    Updates run-release-uberjar script

--- scripts/run-release-uberjar.sh
+++ scripts/run-release-uberjar.sh
@@ -8,7 +8,8 @@ export SESSION_COOKIE_name="cookie-name"
 export CLJS_BUILD_ID=release 
 export SERVER_URL_SCHEMA="http"
 export SERVER_URL_HOST="localhost:3000"
+export STRIPE_SECRET_KEY=stripe-secret
 
-cd "$script_dir"
+cd "$script_dir/.."
 lein uberjar
 java -server -Xmx4g -Dclojure.compiler.direct-linking=true -cp ./target/uberjar/budget-0.1.0-SNAPSHOT-standalone.jar eponai.server.core
