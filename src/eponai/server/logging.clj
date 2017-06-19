401c4915eaefec906bf62d1e971875e5a9a2eb09
Petter Eriksson <ipetterik@gmail.com>
2017-06-18 18:13:51 -0700

    Adds server logging namespace, for logging that matters

---
+++ src/eponai/server/logging.clj
@@ -0,0 +1,76 @@
+(ns eponai.server.logging
+  (:require
+    [taoensso.timbre :as timbre]
+    [clojure.core.async :as async]
+    [eponai.common.format.date :as date]
+    [com.stuartsierra.component :as component]))
+
+(defprotocol ILogger
+  (log [this msg]))
+
+;; ------- Log manipulation functions
+
+(defn with [logger f]
+  (reify
+    ILogger
+    (log [_ msg]
+      (log logger (f msg)))))
+
+(defn- logging-thread [logger in]
+  (async/thread
+    (loop []
+      (when-some [v (async/<!! in)]
+        (try
+          (log logger v)
+          (catch Throwable e
+            (try
+              (timbre/error ::async-error
+                            e
+                            {:exception-class   (.getClass e)
+                             :exception-message (.getMessage e)})
+              (catch Throwable _))))
+        (recur)))))
+
+(defn async-logger
+  ([logger]
+   (async-logger logger 1))
+  ([logger threads]
+   (let [capacity 100
+         buf (async/sliding-buffer capacity)
+         msg-chan (async/chan buf)]
+     (take threads (repeatedly #(logging-thread logger msg-chan)))
+     (reify
+       ILogger
+       (log [_ msg]
+         (when (== capacity (count buf))
+           (timbre/debug "logging capacity has been reached. Will drop message."))
+         (async/put! msg-chan msg))
+       component/Lifecycle
+       (stop [this]
+         (async/close! msg-chan))))))
+
+;; ------ Logging functions
+
+(defn- log! [level logger id data]
+  {:pre [(and (keyword? id) (some? (namespace id)))
+         (map? data)]}
+  (log logger {:id id
+               :level level
+               :data data
+               :millis (System/currentTimeMillis)}))
+
+(defn info [logger id data]
+  (log! :info logger id data))
+
+(defn warn [logger id data]
+  (log! :warn logger id data))
+
+(defn error [logger id data]
+  (log! :error logger id data))
+
+;; ------ Loggers
+
+(defrecord TimbreLogger []
+  ILogger
+  (log [this msg]
+    (timbre/log (:level msg) (:id msg) " " (dissoc msg :level :id :millis))))
