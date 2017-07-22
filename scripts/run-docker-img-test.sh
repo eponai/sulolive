0b2b8896971a7b46a372d0f70b987be3b4237995
Petter Eriksson <ipetterik@gmail.com>
2017-07-21 21:25:38 -0700

    Doesn't need to select --network for our usage of curl and containers

--- scripts/run-docker-img-test.sh
+++ scripts/run-docker-img-test.sh
@@ -5,5 +5,5 @@ PORT=$2
 ENDPOINT=/
 
 echo "Test with forwarding (which in production is done by load balancer)"
-docker run --network container:contacts appropriate/curl -H 'x-forwarded-proto: https' --retry 30 --retry-delay 1 --retry-connrefused http://$HOST:$PORT$ENDPOINT
+docker run appropriate/curl -H 'x-forwarded-proto: https' --retry 30 --retry-delay 1 --retry-connrefused http://$HOST:$PORT$ENDPOINT
 
