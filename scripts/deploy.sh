5416c1201f9ee35a715e53f810b3633284d2e19e
Petter Eriksson <ipetterik@gmail.com>
2017-08-07 16:18:28 -0400

    Runs with different amounts of memory in production and on master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,11 @@ ASSET_VERSION=$(echo "$SHA1" | cut -c1-10)
 
 # Set branch specific variables
 if [ "${CIRCLE_BRANCH}" = "production" ]; then
+  WEBSERVER_MEMORY=1500
   EB_ENV_NAME="$(scripts/_env-name-of-current-staging.sh)"
   FIREBASE_SERVICE_ACCOUNT="$FIREBASE_SERVICE_ACCOUNT_PRODUCTION"
 elif [ "${CIRCLE_BRANCH}" = "petter/WEB-233-circle-ci-2" ]; then
+  WEBSERVER_MEMORY=700
   EB_ENV_NAME="sulo-master"
   FIREBASE_SERVICE_ACCOUNT="$FIREBASE_SERVICE_ACCOUNT_MASTER"
 else
@@ -33,7 +35,13 @@ DOCKERRUN_S3_FILE="docker/$EB_APP_NAME/$EB_ENV_NAME/$DOCKERRUN_FILE"
 
 # Create dockerrun file for elastic beanstalk
 # Using comma (,) instead of slash (/) in sed because DOCKER_IMAGE contains slashes
-cat Dockerrun2.aws.json.template | sed "s,<ASSET_VERSION>,$ASSET_VERSION," | sed  "s,<DOCKER_IMAGE>,$DOCKER_IMAGE," | sed  "s,<DATADOG_API_KEY>,$DATADOG_API_KEY," | sed  "s,<FIREBASE_SERVICE_ACCOUNT>,$FIREBASE_SERVICE_ACCOUNT," > $DOCKERRUN_FILE
+cat Dockerrun2.aws.json.template \
+    | sed "s,<ASSET_VERSION>,$ASSET_VERSION," \
+    | sed "s,<DOCKER_IMAGE>,$DOCKER_IMAGE," \
+    | sed "s,<WEBSERVER_MEMORY>,$WEBSERVER_MEMORY," \
+    | sed "s,<DATADOG_API_KEY>,$DATADOG_API_KEY," \
+    | sed "s,<FIREBASE_SERVICE_ACCOUNT>,$FIREBASE_SERVICE_ACCOUNT," \
+    > $DOCKERRUN_FILE
 
 aws --region=$REGION s3 cp "$DOCKERRUN_FILE" "s3://$EB_BUCKET/$DOCKERRUN_S3_FILE"
 aws --region=$REGION elasticbeanstalk create-application-version --application-name "$EB_APP_NAME" \
