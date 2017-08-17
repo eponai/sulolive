9d4bb5fe6312c069f88f1c2d06a7b88b2ea86e21
Petter Eriksson <ipetterik@gmail.com>
2017-08-17 16:52:37 -0400

    Prepares to deploy our new firebase functions to prod

--- scripts/deploy-firebase-functions.sh
+++ scripts/deploy-firebase-functions.sh
@@ -1,6 +1,16 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 script_dir=$(dirname $0)
 
+if [ -z "$1" ]; then
+  echo "No project was passed. Defaulting to our dev project."
+  project=leafy-firmament-160421
+else
+  project="$1"
+fi
+
+
 cd "$script_dir/../firebase"
-firebase deploy --only functions
+
+echo "Deploying functions to project: $project"
+firebase --project "$project" deploy --only functions
