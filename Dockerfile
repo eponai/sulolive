36a231101f1bf74a9fdf840f75fd0da275ab51fe
Petter Eriksson <periksson@splunk.com>
2017-08-17 14:10:42 -0400

    Petter/add bower css to uberjar (#133)
    * Updates circleci, dockerfile and compile-css with new bower css library
    
    * Removes invalid script from circleci run command
    
    * Updates path to flag css from bower_components to resources/public/assets
    
    * Creates the assets/css dir if it ddoes't exist when compiling css
    
    * build_css persists both flags and css assets

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM openjdk:8-jdk
 
 ADD target/uberjar/budget-uberjar.jar /srv/production.jar
-ADD resources/public/assets/css/app.css /srv/public/assets/css/app.css
+ADD resources/public/assets/css /srv/public/assets/css
+ADD resources/public/assets/flags /srv/public/assets/flags
 ADD resources/public/release/js/out/budget.js /srv/public/release/js/out/budget.js
 
 EXPOSE 8080
@@ -9,7 +10,8 @@ EXPOSE 8080
 WORKDIR "/srv"
 
 RUN jar cf assets.jar \
-    public/assets/css/app.css \
+    public/assets/css \
+    public/assets/flags \
     public/release/js/out/budget.js
 
 CMD exec java $JAVA_OPTS -server -Dclojure.compiler.direct-linking=true -cp production.jar:assets.jar eponai.server.core
--- scripts/compile-css.sh
+++ scripts/compile-css.sh
@@ -1,34 +1,43 @@
-#!/bin/bash -u
+#!/bin/bash -eu
 
 script_dir=$(dirname $0)
-foundation_dir="$script_dir/../sulo-style"
+proj_dir="$script_dir/.."
+foundation_dir="$proj_dir/sulo-style"
 
-foundation_install=$(which foundation)
-exit_code=$?
-if [ "0" != "$exit_code" ]; then
-  echo "Could not find foundation. Installing globally"
-  npm install --global foundation-cli
-else
-  echo "Using foundation: $foundation_install"
-fi
+FOUNDATION="node ../node_modules/foundation-cli/bin/foundation.js" 
+BOWER="node_modules/bower/bin/bower"
 
-bower_install=$(which bower)
-if [ "0" != "$?" ]; then
-  echo "Could not find bower. Installing globally"
-  npm install -g bower
-else
-  echo "Using bower: $bower_install"
+set +u
+if [ -z "$SKIP_INSTALL" ]; then
+  npm install
+  $BOWER install --allow-root
+  ( cd "$foundation_dir" \
+    && npm install \
+    && "../$BOWER" install --allow-root )
 fi
+set -u
+
+#######################
+## Put bower stuff in resources
+
+assets_dir="resources/public/assets"
+css_dir="$assets_dir/css"
+flags_dir="$assets_dir/flags"
+mkdir -p $css_dir
+cp -r bower_components/flag-icon-css/css/flag-icon.min.css "$css_dir/flag-icon.min.css"
+mkdir -p "$flags_dir"
+cp -r bower_components/flag-icon-css/flags/* "$flags_dir"
+
+######################
+## Call foundation with arguments passed to this script (or build)
 
 cd "$foundation_dir"
-npm install
-bower install
 
 if [[ $# -ge 1 ]]; then
   echo "Calling: foundation $@"
-  foundation $@
+  $FOUNDATION $@
 else
   echo "Calling: foundation build"
-  foundation build
+  $FOUNDATION build
 fi
 
