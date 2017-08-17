d603b41dd7d831bf68c8d60e032111c2d945ed44
Petter Eriksson <ipetterik@gmail.com>
2017-08-17 17:43:03 -0400

    Environment variable to allow the server to use http

--- src/eponai/server/core.clj
+++ src/eponai/server/core.clj
@@ -27,7 +27,9 @@
 
 (defn -main [& _]
   (try
-    (let [system (component/start-system (make-system {::system-fn system/prod-system}))
+    (let [system (component/start-system (make-system (cond-> {::system-fn system/prod-system}
+                                                              (env/env :server-allow-http)
+                                                              (assoc ::system/disable-ssl true))))
           server (get-in system [:system/aleph :server])]
       (mixpanel/set-token ::mixpanel/token-release)
       (aleph.netty/wait-for-close server))
